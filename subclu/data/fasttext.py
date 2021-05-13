77a44bb4b6be82e2ad78dc183ed79b6420a33a10
David Bermejo <david.bermejo@reddit.com>
2021-05-13 02:29:43 -0500

    Update util to compare fastText word similarity

--- subclu/data/fasttext.py
+++ subclu/data/fasttext.py
@@ -4,9 +4,12 @@ This is a wrapper around fastText's utilities that makes sure to save embeddings
 to a default location.
 """
 
-from pathlib import Path
 from copy import deepcopy
+from pathlib import Path
+from typing import List
+
 import fasttext.util
+import pandas as pd
 
 from subclu.utils import set_working_directory
 
@@ -64,6 +67,49 @@ def download_ft_pretrained_model(
         rel_file_name = fasttext.util.download_model(lang_id, if_exists=if_exists)
 
     return path_ft_embeddings / rel_file_name
+
+
+def get_df_for_most_similar(
+        ft_model,
+        list_of_words: List[str],
+        print_oov_check: bool = True,
+) -> pd.DataFrame:
+    """
+    Take a list of words and return a df to more easily compare the most similar words side by side.
+
+    By default, "most_similar" returns a list of tuples which is not great for visualization & comparing.
+    """
+    l_sim = list()
+
+    for word_ in list_of_words:
+        l_this_word_ = ft_model.most_similar(word_)
+        l_sim.append({
+            f"'{word_}' similar_words": [t[0] for t in l_this_word_],
+            f"'{word_}' similarity_score": [t[1] for t in l_this_word_],
+        })
+        del l_this_word_, word_
+
+    if print_oov_check:
+        for word_ in list_of_words:
+            print(f"{word_ in ft_model} -> {word_} in vocabulary?")
+
+    df = pd.DataFrame(l_sim[0])
+
+    if 1 == len(l_sim):
+        return df
+    else:
+        for i, l_word_ in enumerate(l_sim[1:], start=1):
+            df = (
+                df
+                .assign(**{' ' * i: ['|'] * len(df)})
+                .merge(
+                    pd.DataFrame(l_word_),
+                    how='outer',
+                    left_index=True,
+                    right_index=True,
+                )
+            )
+        return df
 
 
 #
