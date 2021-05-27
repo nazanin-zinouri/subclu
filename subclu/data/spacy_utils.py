40c772bd423acbb4d7589cfd072a4bdd9ff154bc
David Bermejo <david.bermejo@reddit.com>
2021-05-26 23:03:33 -0500

    Add notes in utility file to download spacy models

---
+++ subclu/data/spacy_utils.py
@@ -0,0 +1,19 @@
+"""
+Utils to download spacy models, similar to fasttext_utils
+
+For now, download manually from the command line:
+
+```
+# Spacy will download models to its default location
+python -m spacy download de_core_news_lg
+python -m spacy download en_core_web_lg
+```
+
+For reference:
+https://spacy.io/models/de#de_core_news_lg
+- lg ~ 546MB
+
+https://spacy.io/models/en
+- lg ~ 742MB
+"""
+
