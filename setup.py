2a47d45b16e95c296d79d60ed07ae88241fed86e
David Bermejo <david.bermejo@reddit.com>
2021-07-15 09:20:45 -0500

    Clean up setup.py - move more libraries to EXTRA specific locations
    e.g., some libraries are only used in CPU/EDA/ Large RAM VM so only install them there.

--- setup.py
+++ setup.py
@@ -8,52 +8,67 @@ from setuptools import find_packages, setup
 
 # These libraries get installed always
 INSTALL_REQUIRES = [
-    # cookie cutter tools
-    # "python-dotenv >= 0.5.1",
-    # For some reason leaving click unpinned creates weird conflicts
-    # "click <= 7.1.2 ",
-
+    # For some reason leaving `click` (& a few other libraries) unpinned creates
+    #  weird conflicts might need to pin different versions for each VM, though
     # google VM pins
     "jinja2 <= 2.11.3",
-    "pydantic <= 1.8.2",
     "markupsafe <= 1.1.1",
+    "pydantic <= 1.8.2",
 
     # DS/ML core
+    # pandas, np, and sklearn versions are managed by Google notebooks and
+    #  are not guaranteed to be the same, so don't pin them to prevent errors
+    "mlflow == 1.16.0",
     # "pandas == 1.2.4",
     # "scikit-learn == 0.24.1",
     # "joblib == 1.0.1",
-    # np version might not be tf2 version
     # "numpy == 1.19.5",
-    "mlflow == 1.16.0",
-    "dask == 2021.6.0",
-    "modin == 0.10.0",
+
+    # Exclude dask & modin while I test out spark
+    "dask[complete] == 2021.6.0",
+    # "modin == 0.10.0",
+    # "modin[dask]",
+    #  "ray",
 
     # Auth
-    "pydata-google-auth",
+    # "pydata-google-auth",
 
     # NLP
+    # for v0.3 I'm no longer using fse, fasttext or spacy,
+    #  Instead I'm using USE-multilingual & tensorflow, so move them
+    #  to cpu-only extras to reduce conflicts & reduce install time
     "gensim == 3.8.3",  # 4.0 is not compatible with fse
-    "fse == 0.1.15",
-    "fasttext == 0.9.2",
-    "spacy == 3.0.5",
 
     # Visualization
     "seaborn == 0.11.1",
     "plotly == 4.14.3",
     "kaleido == 0.2.1",
-    "umap-learn == 0.5.1",
-    "openTSNE == 0.6.0",
 
 ]
 
 # These libraries will be installed only if called with "extras" syntax:
 #   $ pip install sampleproject[dev]
 EXTRAS_REQUIRE = {
-    "cookiecutter": ["Sphinx", "coverage", "awscli", "flake8"],
-    "devcpu": [
-        "jupyterlab >= 1.2.16",
+    "cookiecutter": [
+        # cookie cutter tools
+        "python-dotenv >= 0.5.1",
+        "Sphinx", "coverage", "awscli", "flake8"
+    ],
+
+    "cpu_eda": [
         "ipython == 7.22.0",
+        "jupyterlab == 1.2.16",
         "pyarrow == 3.0.0",
+
+        # NLP / embeddings
+        "fasttext == 0.9.2",
+        "fse == 0.1.15",
+        "spacy == 3.0.5",
+
+        # compression / visualization
+        "umap-learn == 0.5.1",
+        "openTSNE == 0.6.0",
+
     ],
 
     "pytorch": [
@@ -64,9 +79,10 @@ EXTRAS_REQUIRE = {
 
     # tensorflow = extra libraries needed to run TF models
     # for some reason there's a numpy conflict with base install that
-    # interferes with tf==2.3.2
+    # can interfere with tf==2.3.2
     "tensorflow_232": [
         "click <= 8.0.1",
+        "numpy == 1.19.5",
 
         "tensorflow == 2.3.2",
         # "tensorflow-cloud == 0.1.13",
@@ -80,8 +96,10 @@ EXTRAS_REQUIRE = {
         # "tensorflow-probability == 0.11.0",
         # "tensorflow-serving-api == 2.3.0",
         # "tensorflow-transform == 0.26.0",
+
         "tensorflow-text == 2.3.0",
-        "Werkzeug == 2.0.1"
+
+        "Werkzeug == 2.0.1",
     ],
 
     "tensorflow_233": [
@@ -99,6 +117,7 @@ EXTRAS_REQUIRE = {
         "tensorflow-probability == 0.11.0",
         "tensorflow-serving-api == 2.3.0",
         "tensorflow-transform == 0.26.0",
+
         "tensorflow-text == 2.3.0",
     ],
 
