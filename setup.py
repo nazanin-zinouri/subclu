52f4adf3986de354f03bae9072b33992099f36ab
David Bermejo <david.bermejo@reddit.com>
2021-08-02 22:21:09 -0500

    Update setup.py for GPU machine and hydra to manage configs

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ Reference setup files
 https://github.com/pypa/sampleproject/blob/main/setup.py
 https://github.com/dask/dask/blob/main/setup.py
 """
-
 from setuptools import find_packages, setup
 
 # These libraries get installed always
@@ -109,8 +108,47 @@ EXTRAS_REQUIRE = {
     ],
 
     "tensorflow_233": [
-        "click <= 7.1.2",
+        # Core libraries need to be pinned... otherwise `pip` tries to upgrade them
+        "click == 8.0.1",
+        "joblib == 1.0.1",
+        "pyarrow == 4.0.1",
+        "numpy == 1.19.5",
 
+        # Google pre-installed/built-in services
+        #  For some reason, pip wanted to update a bunch of these
+        #  when installing hydra-core
+        "google-api-core == 1.30.0",
+        "google-api-python-client == 2.10.0",
+        "google-apitools == 0.5.31",
+        "google-auth == 1.30.2",
+        "google-auth-httplib2 == 0.1.0",
+        "google-auth-oauthlib == 0.4.4",
+        "google-cloud-appengine-logging == 0.1.1",
+        "google-cloud-audit-log == 0.1.0",
+        "google-cloud-bigquery == 2.20.0",
+        "google-cloud-bigquery-storage == 2.4.0",
+        "google-cloud-bigtable == 2.2.0",
+        "google-cloud-build == 2.0.0",
+        "google-cloud-core == 1.7.0",
+        "google-cloud-dataproc == 2.4.0",
+        "google-cloud-datastore == 2.1.3",
+        "google-cloud-dlp == 1.0.0",
+        "google-cloud-firestore == 2.1.3",
+        "google-cloud-kms == 2.3.0",
+        "google-cloud-language == 2.1.0",
+        "google-cloud-logging == 2.5.0",
+        "google-cloud-monitoring == 2.3.0",
+        "google-cloud-pubsub == 1.7.0",
+        "google-cloud-scheduler == 2.2.0",
+        "google-cloud-spanner == 3.5.0",
+        "google-cloud-speech == 2.4.1",
+        "google-cloud-storage == 1.39.0",
+        "google-cloud-tasks == 2.3.0",
+        "google-cloud-translate == 3.2.1",
+        "google-cloud-videointelligence == 2.2.0",
+        "google-cloud-vision == 2.3.2",
+
+        # TF pre-installed libraries
         "tensorflow == 2.3.3",
         "tensorflow-cloud == 0.1.13",
         "tensorflow-data-validation == 0.26.1",
@@ -124,7 +162,11 @@ EXTRAS_REQUIRE = {
         "tensorflow-serving-api == 2.3.0",
         "tensorflow-transform == 0.26.0",
 
+        # TF library needed to use USE-multilingual
         "tensorflow-text == 2.3.0",
+
+        # Use hydra to manage config files
+        "hydra-core == 1.1.0",
     ],
 
 }
