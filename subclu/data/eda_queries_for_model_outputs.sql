cd540508de1e9dadfd439585a0614ed6f9887ace
David Bermejo <david.bermejo@reddit.com>
2021-08-06 13:51:09 -0500

    Update EDA queries for modeling outputs

--- subclu/data/eda_queries_for_model_outputs.sql
+++ subclu/data/eda_queries_for_model_outputs.sql
@@ -10,19 +10,26 @@ GROUP BY 1
 ORDER BY 2 DESC
 ;
 
--- Get most similar sub, given an input sub name
+-- Get most similar subs, given an input sub name
+DECLARE SUB_NAME STRING;
+SET SUB_NAME = 'bundesliga';
+
+-- Get most similar subs, given an input sub name
 SELECT * EXCEPT(subreddit_id_a, subreddit_id_b)
 FROM `reddit-employee-datasets.david_bermejo.subclu_subreddit_distance_v0031_german_c_posts_and_comments_and_meta`
-WHERE subreddit_name_a = 'bundesliga'
+WHERE subreddit_name_a = SUB_NAME
 ;
 
 
 -- Get the cluster ID & cluster subs, given an input sub name
+DECLARE SUB_NAME STRING;
+SET SUB_NAME = 'bundesliga';
+
 WITH cluster_for_selected_sub AS(
 SELECT
     cluster_id_agg_ward_cosine_35
 FROM `reddit-employee-datasets.david_bermejo.subclu_subreddit_cluster_labels_v031_a`
-WHERE subreddit_name = 'bundesliga'
+WHERE subreddit_name = SUB_NAME
 )
 
 SELECT
@@ -48,6 +55,62 @@ INNER JOIN cluster_for_selected_sub AS sel
     ON lbl.cluster_id_agg_ward_cosine_35 = sel.cluster_id_agg_ward_cosine_35
 
 WHERE manual_topic_and_rating != 'over18_nsfw'
+ORDER BY cluster_id_agg_ward_cosine_35 ASC, users_l28 DESC
+;
+
+
+-- Find out which subs have been rated
+--  For now, it's static labels, but will need to join with live data (maybe a view?)
+-- SELECT
+--     whitelist_status
+--     , COUNT(DISTINCT subreddit_name) AS unique_subreddit_count
+-- FROM `reddit-employee-datasets.david_bermejo.subclu_subreddit_cluster_labels_v031_a`
+-- GROUP BY 1
+-- ORDER BY 2 DESC
+-- ;
+
+SELECT
+    rating
+    -- , whitelist_status
+    , COUNT(DISTINCT subreddit_name) AS unique_subreddit_count
+FROM `reddit-employee-datasets.david_bermejo.subclu_subreddit_cluster_labels_v031_a`
+GROUP BY 1
+ORDER BY 2 DESC
+;
+
+SELECT
+    rating
+    , whitelist_status
+    , COUNT(DISTINCT subreddit_name) AS unique_subreddit_count
+FROM `reddit-employee-datasets.david_bermejo.subclu_subreddit_cluster_labels_v031_a`
+GROUP BY 1, 2
+ORDER BY 1 ASC, 3 DESC
+;
+
+
+-- Select key cols for subs that are in the allow-list for Ads
+SELECT
+    subreddit_name
+    , subreddit_id
+    , manual_topic_and_rating
+    , lbl.cluster_id_agg_ward_cosine_35
+    , post_median_word_count
+    , German_posts_percent
+    , subreddit_language
+    , posts_l28
+    , subscribers
+    , users_l7
+    , users_l28
+    , subreddit_title
+    , subreddit_public_description
+
+    , svd_0
+    , svd_1
+    , svd_2
+
+FROM `reddit-employee-datasets.david_bermejo.subclu_subreddit_cluster_labels_v031_a` AS lbl
+
+WHERE whitelist_status IN ('all_ads', 'some_ads')
 ORDER BY cluster_id_agg_ward_cosine_35 ASC, users_l28 DESC
 ;
 
