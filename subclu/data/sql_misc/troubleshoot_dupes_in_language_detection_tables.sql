1d4f541879f332985af21adbaa8b89527e78f7bd
David Bermejo <david.bermejo@reddit.com>
2021-09-08 18:16:51 -0500

    Add scratch queries to get unique comment counts

--- subclu/data/sql_misc/troubleshoot_dupes_in_language_detection_tables.sql
+++ subclu/data/sql_misc/troubleshoot_dupes_in_language_detection_tables.sql
@@ -248,3 +248,159 @@ FROM post_count_per_year
 -- COMMENT level doesn't have an equivalent table to `post_lookup`
 -- Using `successful_comment` returns a very small number of posts, not sure why/how
 -- is it because they're filtering out spam or something else?
+-- Get unique comments in `successful_comments` table
+-- Looks like `data-prod-165221.cnc.successful_comments` also has some duplicates
+DECLARE pt_start_date DATE DEFAULT '2015-01-01';
+DECLARE pt_end_date DATE DEFAULT '2021-09-07';
+
+WITH comments_ranked AS (
+SELECT
+    -- Use row_number to get the latest edit as row=1
+    ROW_NUMBER() OVER (
+        PARTITION BY post_id
+        ORDER BY endpoint_timestamp DESC, removal_timestamp desc
+    ) AS row_num
+    , removal_timestamp
+    , removed
+    , * EXCEPT(removal_timestamp, removed)
+
+FROM `data-prod-165221.cnc.successful_comments` AS sc
+
+WHERE sc.dt BETWEEN pt_start_date AND pt_end_date
+    -- AND comment_id IN (
+    --     -- w/o dupes
+    --     't1_hbq0t4q', 't1_hbmzuzk', 't1_hbo17hj', 't1_hbmwjyb'
+
+    --     -- with dupes
+    --     , 't1_hbma5hj', 't1_hbl8kh0', 't1_hboyjd1'
+
+    --     -- with dupes AND removed=0
+    --     , 't1_hbq752z', 't1_hbqgi1k'
+    -- )
+
+),
+
+comment_count_per_year AS (
+SELECT
+    EXTRACT(YEAR from endpoint_timestamp) AS YEAR
+    , COUNT(*) total_rows
+    , COUNT(DISTINCT comment_id) AS comment_id_unique
+    , SUM(
+        CASE WHEN (removed = 1) THEN 1
+            ELSE 0
+        END
+    ) AS comment_ids_removed
+    , SUM(
+        CASE WHEN (removed = 0) THEN 1
+            ELSE 0
+        END
+    ) AS comment_ids_active
+
+FROM comments_ranked
+
+WHERE 1=1
+    AND row_num = 1
+
+GROUP BY 1
+ORDER BY 1
+)
+
+-- Counts PER YEAR
+SELECT
+    *
+FROM comment_count_per_year
+;
+
+-- Try DISTINCT: it doesn't remove any duplicates on all columns
+-- SELECT
+--     DISTINCT * EXCEPT (row_num)
+-- FROM comments_unique
+
+-- ORDER BY comment_id, endpoint_timestamp DESC
+-- ;
+
+-- If we sort by removal date, what happens if removal date is blank?
+-- SELECT
+--     *
+-- FROM comments_unique
+
+-- ORDER BY post_id, row_num
+
+-- LIMIT 200
+-- ;
+
+-- HOWEVER, it looks like we can find duplicates if we exclude: removal_timestamp & endpoint_timestamp
+--  it appears that these cols that can change over time :mind-blown: sigh
+--  so we should probably row_num() over removed time stamp and keep the latest action
+-- SELECT
+--     DISTINCT * EXCEPT(removal_timestamp, row_num)
+-- FROM comments_unique
+
+-- ORDER BY comment_id, endpoint_timestamp DESC
+-- ;
+
+-- We can also get uniques by picking only row_num = 1
+-- SELECT
+--     *
+-- FROM comments_unique
+-- WHERE row_num = 1
+-- ;
+
+-- ALL Counts using new filter
+-- SELECT
+--     COUNT(*) total_rows
+--     , COUNT(DISTINCT comment_id) AS comment_id_unique
+--     , SUM(
+--         CASE WHEN (removed = 0) THEN 1
+--             ELSE 0
+--         END
+--     ) AS comment_ids_active
+--     , SUM(
+--         CASE WHEN (removed = 1) THEN 1
+--             ELSE 0
+--         END
+--     ) AS comment_ids_removed
+-- FROM comments_unique
+-- WHERE row_num = 1
+-- ;
+
+-- =====================
+-- Other calls w/o CTE
+-- ===
+-- Find COMMENTS that are duplicated or NOT duplicated
+-- SELECT
+--     comment_id
+--     , COUNT(*)  AS row_count
+
+-- FROM `data-prod-165221.cnc.successful_comments` AS sc
+
+-- WHERE sc.dt BETWEEN pt_start_date AND pt_end_date
+--     AND removed = 0
+
+-- GROUP BY 1
+
+-- ORDER BY 2 DESC
+-- LIMIT 1500
+-- ;
+
+
+-- Check whether successful comments table also has duplicates
+--  Answer: yes
+-- SELECT
+--     COUNT(*) total_rows
+--     , COUNT(DISTINCT comment_id) AS comment_id_unique
+--     , SUM(
+--         CASE WHEN (removed = 0) THEN 1
+--             ELSE 0
+--         END
+--     ) AS comment_ids_active
+--     , SUM(
+--         CASE WHEN (removed = 1) THEN 1
+--             ELSE 0
+--         END
+--     ) AS comment_ids_removed
+
+-- FROM `data-prod-165221.cnc.successful_comments` AS sc
+
+-- WHERE sc.dt BETWEEN pt_start_date AND pt_end_date
+-- ;
