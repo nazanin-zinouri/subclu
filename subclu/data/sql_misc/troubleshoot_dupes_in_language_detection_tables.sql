8b49449a7197c2efb6c0d76610f8678e21eaf92b
David Bermejo <david.bermejo@reddit.com>
2021-09-27 13:01:43 -0500

    Update query to remove duplicates in successful posts AND language post detection tables

--- subclu/data/sql_misc/troubleshoot_dupes_in_language_detection_tables.sql
+++ subclu/data/sql_misc/troubleshoot_dupes_in_language_detection_tables.sql
@@ -7,6 +7,160 @@
 -- - `data-prod-165221.language_detection.post_language_detection_cld3`
 -- - data-prod-165221.language_detection.comment_language_detection_cld3
 
+-- ========================
+-- 2021-09-27
+-- WIP here's how to remove duplicates
+--
+-- Clean language-v2 table to drop duplicate posts
+-- NEW:
+--   * use `post_thing_user_row_num` = 1 to keep only 1 row in language table
+--   * use `row_num` = 1 to keep only 1 row in successful_posts table
+--  Use it to make accurate & faster JOINS when counting languge posts & selecting posts
+--  for semantic clusters
+DECLARE partition_date DATE DEFAULT '2021-09-05';
+
+-- Dates for edited post:
+DECLARE pt_start_date DATE DEFAULT '2021-09-03';
+DECLARE pt_end_date DATE DEFAULT '2021-09-08';
+
+
+WITH posts_with_language AS (
+    SELECT
+        DATE(_PARTITIONTIME) as pt
+        -- `thing_order_in_post` doesn't work:
+        --  There's no good way to sort at POST-ID only because the `created_timestamp` is the same for all duplicates
+        --  the only differences are: user_id, app_name (of commenters), geo_location_country_code (of commenters)
+        --    earliest=(created_timestamp ASC) or latest=(created_timestamp DESC)
+
+        -- Instead, rank by post-ID + user_id + thing_type (one user can post AND comment)
+        --   If/when we want comment-level we need to add ID (comment ID) INTO THE partition
+        , ROW_NUMBER() OVER(
+            PARTITION BY post_id, id, user_id
+            ORDER BY created_timestamp ASC, probability DESC
+        ) AS post_thing_user_row_num
+        , ROW_NUMBER() OVER(
+            PARTITION BY post_id
+            ORDER BY thing_type DESC, created_timestamp ASC
+        ) AS thing_order_in_post_asc
+        , ROW_NUMBER() OVER(
+            PARTITION BY post_id
+            ORDER BY thing_type DESC, created_timestamp DESC
+        ) AS thing_order_in_post_desc
+        -- , *
+        , created_timestamp
+        , subreddit_id
+        , post_id
+        , user_id
+        , id AS post_or_comment_id
+
+        , thing_type
+        , app_name
+        , geolocation_country_code
+        , weighted_language
+        , language
+        , probability
+        , weighted_language_probability
+
+        , text
+
+    FROM `reddit-protected-data.language_detection.comment_language_v2`
+    WHERE DATE(_PARTITIONTIME) BETWEEN pt_start_date AND pt_end_date
+        AND thing_type = 'post'
+        AND id = post_id
+    ),
+
+    successful_posts AS(
+        SELECT
+            -- Use row_number to get the latest edit as row=1
+            ROW_NUMBER() OVER (
+                PARTITION BY post_id
+                ORDER BY endpoint_timestamp DESC, removal_timestamp DESC
+            ) AS row_num
+            , removal_timestamp
+            , removed
+            , * EXCEPT(removal_timestamp, removed)
+
+        FROM `data-prod-165221.cnc.successful_posts` AS sp
+
+        WHERE sp.dt BETWEEN pt_start_date AND pt_end_date
+
+        ORDER BY post_id, row_num
+    )
+
+
+-- for a post known to have duplicates:
+-- GROUP BY thing-type (post v comment)
+--  count rows
+SELECT
+    -- get counts only
+    COALESCE(sp.post_id, pl.post_id) AS post_id
+    , pl.thing_type
+    , COUNT(*) AS row_count
+    , COUNT(DISTINCT pl.post_or_comment_id ) AS post_or_comment_id_unique
+    , COUNT(DISTINCT sp.user_id ) AS user_id_unique
+
+    -- check individual rows
+    -- sp.subreddit_name
+    -- , COALESCE(sp.post_id, pl.post_id) AS post_id
+    -- , COALESCE(sp.user_id, pl.user_id) AS user_id
+    -- , pl.thing_order_in_post_asc
+    -- , pl.thing_order_in_post_desc
+    -- , pl.created_timestamp
+    -- , pl.pt
+    -- , pl.post_thing_user_row_num
+
+    -- , sp.row_num
+    -- , sp.removal_timestamp
+    -- , sp.endpoint_timestamp
+    -- , sp.dt
+    -- , sp.submit_date
+
+    -- , sp.post_title
+    -- , LEFT(pl.text, 50) AS text_first_50
+    -- , pl.* EXCEPT(thing_order_in_post_desc, thing_order_in_post_asc, created_timestamp, text)
+    -- , sp.* EXCEPT(row_num, removal_timestamp, endpoint_timestamp, dt, submit_date, post_body_text)
+
+FROM posts_with_language AS pl
+INNER JOIN successful_posts AS sp
+    ON pl.post_id = sp.post_id AND pl.user_id = sp.user_id
+        AND pl.app_name = sp.app_name
+        AND pl.thing_type = sp.noun
+WHERE 1=1
+    -- Remove duplicates in successful_post table (multiple rows when a post is removed multiple times)
+    AND sp.row_num = 1
+
+    -- Remove dupes in language detection table at POST-LEVEL
+    -- Note: might NEED A SEPARATE COL FOR COMMENT-LEVEL DUPES
+    AND pl.post_thing_user_row_num = 1
+
+    AND (
+        -- posts with some duplicates expected
+        COALESCE(sp.post_id, pl.post_id) IN (
+            't3_pi4jz7', 't3_pijlpp', 't3_phnk2k'
+        )
+        -- post IDs that aren't duplicated
+        OR COALESCE(sp.post_id, pl.post_id) IN (
+            't3_piqzou', 't3_pj61di', 't3_pj9xfo',
+            't3_pj3wld', 't3_pisbia', 't3_pj58h3',
+            't3_pj8nzu', 't3_piuu4j'
+        )
+        -- Too many duplicates 't3_pits1o' (63), 't3_pih5t4' (90 repeats), 't3_phn3iu', 't3_pi2imr'
+        --   use to test FINAL join b/c otherwise it's too noisy to get the uniques
+        -- OR COALESCE(sp.post_id, pl.post_id) IN (
+        --     't3_pits1o', 't3_pih5t4', 't3_phn3iu'
+        -- )
+    )
+
+-- Only apply groupby when counting
+GROUP BY 1, 2
+
+-- Order by when inspecting individual rows
+-- ORDER BY sp.subreddit_id, sp.post_id, thing_type DESC, created_timestamp
+-- ORDER BY sp.post_id, thing_type DESC, created_timestamp
+;
+
+
+
 
 -- ========================
 -- Does new table still have duplicated posts?
