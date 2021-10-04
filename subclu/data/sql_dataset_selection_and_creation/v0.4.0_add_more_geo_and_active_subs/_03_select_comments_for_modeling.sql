0ead47e8cd7c48f4c768dfac74f538d357ae6a19
David Bermejo <david.bermejo@reddit.com>
2021-10-04 12:37:32 -0500

    Add final query for v0.4.0 comments
    Update logic to filter short comments only for posts that have more than max threshold
    Update deduping logic - it speeds up whole query down to ~3 minutes!

--- subclu/data/sql_dataset_selection_and_creation/v0.4.0_add_more_geo_and_active_subs/_03_select_comments_for_modeling.sql
+++ subclu/data/sql_dataset_selection_and_creation/v0.4.0_add_more_geo_and_active_subs/_03_select_comments_for_modeling.sql
@@ -15,17 +15,29 @@
 -- * new GCS folder for new table
 
 -- TODO(djb) Select COMMENTS for v0.4.0 topic clustering
+-- Query complete (3 min 14 sec elapsed, 168.5 GB processed)
 DECLARE start_date DATE DEFAULT '2021-08-01';
 DECLARE end_date DATE DEFAULT '2021-09-21';
-DECLARE MIN_COMMENT_LEN NUMERIC DEFAULT 11;
-DECLARE MAX_COMMENTS_PER_POST NUMERIC DEFAULT 8;
+DECLARE MIN_COMMENT_LEN NUMERIC DEFAULT 8;
+DECLARE MAX_COMMENTS_PER_POST NUMERIC DEFAULT 9;
 
 
--- CREATE OR REPLACE TABLE `reddit-employee-datasets.david_bermejo.subclu_comments_top_no_geo_20210716`
--- PARTITION BY submit_date
--- AS (
+CREATE OR REPLACE TABLE `reddit-employee-datasets.david_bermejo.subclu_comments_top_no_geo_20211004`
+PARTITION BY submit_date
+AS (
 
 WITH
+    selected_posts AS (
+        -- Start with selected posts to reduce orphan comments
+        SELECT * FROM `reddit-employee-datasets.david_bermejo.subclu_posts_top_no_geo_20210927`
+        -- SELECT *
+        -- FROM `reddit-employee-datasets.david_bermejo.subclu_posts_top_no_geo_20210927`
+        -- WHERE 1=1
+        --     AND (
+        --         geo_relevant_subreddit = TRUE
+        --         OR ambassador_subreddit = TRUE
+        --     )
+    ),
     geo AS (
         SELECT
             # Keys & IDS
@@ -57,14 +69,13 @@ WITH
             -- Text
             , sp.comment_body_text
 
-        -- Start with selected posts to reduce orphan comments
-        FROM `reddit-employee-datasets.david_bermejo.subclu_posts_top_no_geo_20210716` AS gs
-        -- FROM (
-        --     SELECT *
-        --     FROM `reddit-employee-datasets.david_bermejo.subclu_posts_top_no_geo_20210716`
-        --     LIMIT 20
-        -- ) AS gs
+            , ROW_NUMBER() OVER (
+                PARTITION BY sp.comment_id
+                ORDER BY endpoint_timestamp DESC, removal_timestamp DESC
+            ) AS row_num_comment_dupes
 
+        -- Start with selected posts to reduce orphan comments
+        FROM selected_posts AS gs
         LEFT JOIN `data-prod-165221.cnc.successful_comments` AS sp
             ON gs.subreddit_name = sp.subreddit_name
                 AND gs.post_id = sp.post_id
@@ -73,6 +84,7 @@ WITH
             AND sp.removed = 0
     ),
 
+    -- TL = thing language. In this case thing=comment
     tl_with_meta AS (
         SELECT
             -- # counts check
@@ -97,7 +109,7 @@ WITH
             , geo.app_name
             , geo.post_type
             , geo.post_nsfw
-            , geo.post_url
+            -- , geo.post_url
             , tl.geolocation_country_code
 
             -- Meta about subreddit
@@ -113,7 +125,7 @@ WITH
             #  Wait to do relatively expensive string manipulation until AFTER removing duplicates
             # , CHAR_LENGTH(tl.text) AS text_len
             # , array_length(regexp_extract_all(tl.text, r"\b\w+\b")) text_word_count_estimate
-            , tl.text AS comment_body_text_for_translation
+            -- , tl.text AS comment_body_text_for_lang_detection
             , geo.comment_body_text
 
             # Metadata to add separately?
@@ -127,13 +139,15 @@ WITH
                 AND thing_type = 'comment'
                 -- AND id !=
         ) AS tl
-        INNER JOIN geo
+        INNER JOIN (
+            SELECT * FROM geo
+            WHERE row_num_comment_dupes = 1
+        ) AS geo
             ON tl.subreddit_id = geo.subreddit_id
                 AND tl.post_id = geo.post_id
                 AND tl.thing_type = geo.noun
                 AND tl.user_id = geo.user_id
-                -- TODO(djb): add to future queries, it might help remove dupes?
-                -- AND tl.id = geo.comment_id
+                AND tl.id = geo.comment_id
 
         -- Exclude some known bots
         WHERE geo.user_id NOT IN ("t2_4kh8rj3k")
@@ -142,7 +156,7 @@ WITH
     tl_unique_with_meta AS
         (
         SELECT
-            * EXCEPT (row_num)
+            * EXCEPT (row_num_tl_dupes)
             , CHAR_LENGTH(comment_body_text) AS comment_text_len
         FROM (
             SELECT
@@ -150,42 +164,234 @@ WITH
                 , ROW_NUMBER() OVER (
                     PARTITION BY post_id, subreddit_id, user_id
                     ORDER BY created_timestamp desc
-                ) row_num
+                ) row_num_tl_dupes
             FROM tl_with_meta
         )
-        WHERE row_num = 1
+        WHERE row_num_tl_dupes = 1
+    ),
+
+    -- If count of comments to post is below threshold, we don't need to filter out or rank posts
+    comments_from_posts_below_threshold AS (
+        SELECT
+            tl.subreddit_id
+            , tl.post_id
+            , tl.comment_id
+
+        FROM (
+            SELECT * FROM selected_posts
+            -- Note that we might end up with a lower number because some comments might be removed
+            WHERE comments <= MAX_COMMENTS_PER_POST
+        )  AS sp
+
+        -- Inner join b/c there might be posts that have all their comments removed
+        INNER JOIN tl_unique_with_meta AS tl
+            ON sp.subreddit_id = tl.subreddit_id AND sp.post_id = tl.post_id
+    ),
+
+    comments_from_posts_above_threshold AS (
+        SELECT
+            tl.subreddit_id
+            , tl.post_id
+            , tl.comment_id
+
+            , ROW_NUMBER() OVER (
+                PARTITION BY tl.post_id
+                ORDER BY tl.upvotes DESC, tl.comment_text_len DESC
+            ) comment_rank_by_post_id
+
+        FROM (
+            SELECT * FROM selected_posts
+            -- Note that we might end up with a lower number because some comments might be removed
+            WHERE comments > MAX_COMMENTS_PER_POST
+        )  AS sp
+
+        -- Inner join b/c there might be posts that have all their comments removed
+        INNER JOIN (
+            SELECT * FROM tl_unique_with_meta
+            -- Filter to keep only comments that are long enough
+            WHERE comment_text_len > MIN_COMMENT_LEN
+        ) AS tl
+            ON sp.subreddit_id = tl.subreddit_id AND sp.post_id = tl.post_id
+    ),
+
+    selected_comments AS (
+        -- Pick only comments that meet ranking/filtering criteria
+        SELECT
+            COALESCE(c1.subreddit_id, c2.subreddit_id) AS subreddit_id
+            , COALESCE(c1.post_id, c2.post_id) AS post_id
+            , COALESCE(c1.comment_id, c2.comment_id) as comment_id
+        FROM comments_from_posts_below_threshold AS c1
+
+        FULL OUTER JOIN (
+            SELECT * FROM comments_from_posts_above_threshold
+            -- filter out comments above threshold
+            WHERE comment_rank_by_post_id <= MAX_COMMENTS_PER_POST
+        ) AS c2
+            ON c1.subreddit_id = c2.subreddit_id
+                AND c1.post_id = c2.post_id
+                AND c1.comment_id = c2.comment_id
     ),
 
     tl_unique_with_meta_top_comments AS (
-        -- Instead of picking all comments, limit to top N comments per post
         SELECT
-            tl.*
-
-        FROM (
-            SELECT
-                ROW_NUMBER() OVER(PARTITION BY post_id ORDER BY upvotes DESC) AS rank_comment_in_post
-                , *
-            FROM tl_unique_with_meta
-            WHERE comment_text_len >= MIN_COMMENT_LEN
-        ) AS tl
-
-        WHERE tl.rank_comment_in_post <= MAX_COMMENTS_PER_POST
-    )
-
-
-    -- This is the de-duped table used for modeling
-    --   Comment this section out if you want to preview with queries below
-    SELECT
-        * EXCEPT (post_url, comment_body_text, comment_body_text_for_translation)
-        , comment_body_text
-
-    FROM (
-        SELECT *
+            tl.* EXCEPT (comment_body_text)
             , array_length(regexp_extract_all(comment_body_text, r"\b\w+\b")) comment_text_word_count
+            , comment_body_text
 
-        FROM tl_unique_with_meta
+        FROM selected_comments AS sc
+        INNER JOIN tl_unique_with_meta AS tl
+            ON sc.subreddit_id = tl.subreddit_id
+                AND sc.post_id = tl.post_id
+                AND sc.comment_id = tl.comment_id
     )
--- ); -- close create table parens
+
+
+    -- This is the final table used for modeling
+    --   Comment this section out if you want to preview with queries below
+    SELECT * FROM  tl_unique_with_meta_top_comments
+); -- close create table parens
+
+
+
+-- ==============================
+-- Count check, tables side by side
+-- ===
+-- SELECT
+--     (SELECT COUNT(*) FROM geo_subs_custom_raw) AS geo_subs_custom_raw_count
+--     , (SELECT COUNT(*) FROM subs_selected_by_geo) AS subs_selected_by_geo_count
+--     , (SELECT COUNT(*) FROM ambassador_subs) AS ambassador_subs_count  -- Expect 173+
+    -- , (SELECT COUNT(*) FROM subs_selected_by_activity) AS subs_selected_by_activity_count_rows
+    -- , (SELECT COUNT(*) FROM selected_subs_base) AS selected_subs_BASE_count_rows
+    -- , (SELECT COUNT(DISTINCT subreddit_id) FROM selected_subs_base) AS selected_subs_BASE_unique_sub_ids_count
+    -- , (SELECT COUNT(*) FROM selected_subs) AS selected_subs_count_rows
+    -- , (SELECT COUNT(DISTINCT subreddit_id) FROM selected_subs) AS selected_subs_unique_sub_ids_count
+--     , (SELECT COUNT(*) FROM final_table) AS final_table_rows_count
+--     , (SELECT COUNT(DISTINCT subreddit_id) FROM final_table) AS final_table_sub_id_unique_count
+-- ;
+
+-- ====================================
+-- Count checks specific tables
+-- ===
+-- Check uniques in tl_unique_with_meta
+-- SELECT
+--     *
+--     , (post_unique_count / subreddit_unique_count) AS posts_per_subreddit_mean
+--     , (comment_unique_count / subreddit_unique_count) AS comments_per_subreddit_mean
+--     , (comment_unique_count / post_unique_count) AS comments_per_post_mean
+
+-- FROM (
+--     SELECT
+--         COUNT(*)       AS row_count
+--         , COUNT(DISTINCT post_id) AS post_unique_count
+--         , COUNT(DISTINCT comment_id) AS comment_unique_count
+--         , COUNT(DISTINCT subreddit_id) AS subreddit_unique_count
+--         -- , COUNT(DISTINCT user_id)   AS user_id_unique
+
+--     FROM tl_unique_with_meta
+-- );
+-- RESULT, only for geo-relevant (including ambassador) subreddits
+--  row_count 	 post_unique_count 	 comment_unique_count 	 subreddit_unique_count 	 posts_per_subreddit_mean 	 comments_per_subreddit_mean 	 comments_per_post_mean
+--  9,831,770 	 652,835 	 9,831,770 	 4,074 	 160.2 	 2,413.3 	 15.1
+
+
+
+-- Counts for final table
+-- SELECT
+--     *
+--     , (post_unique_count / subreddit_unique_count) AS posts_per_subreddit_mean
+--     , (comment_unique_count / subreddit_unique_count) AS comments_per_subreddit_mean
+--     , (comment_unique_count / post_unique_count) AS comments_per_post_mean
+
+-- FROM (
+--     SELECT
+--         COUNT(*)       AS row_count
+--         , COUNT(DISTINCT post_id) AS post_unique_count
+--         , COUNT(DISTINCT comment_id) AS comment_unique_count
+--         , COUNT(DISTINCT subreddit_id) AS subreddit_unique_count
+
+--     FROM tl_unique_with_meta_top_comments
+-- );
+-- RESULTS, only geo-relevant/ambassador subs:
+--    MIN_COMMENT_LEN NUMERIC = 8
+--    MAX_COMMENTS_PER_POST = 9
+--  row_count 	 post_unique_count 	 comment_unique_count 	 subreddit_unique_count 	 posts_per_subreddit_mean 	 comments_per_subreddit_mean 	 comments_per_post_mean
+--  3,566,450 	 652,695 	 3,566,450 	 4,073 	 160.2 	 875.6 	 5.5
+-- RESULTS, all v0.4.0 subreddits. ~ 1:12 elapsed time
+--  row_count 	 post_unique_count 	 comment_unique_count 	 subreddit_unique_count 	 posts_per_subreddit_mean 	 comments_per_subreddit_mean 	 comments_per_post_mean
+--  39,901,968 	 7,038,219 	 39,901,968 	 19,020 	 370.0 	 2,097.9 	 5.7
+
+
+-- Counts for selected comments (this table should have already filtered and ranked the comments!)
+-- SELECT
+--     *
+--     , (post_unique_count / subreddit_unique_count) AS posts_per_subreddit_mean
+--     , (comment_unique_count / subreddit_unique_count) AS comments_per_subreddit_mean
+--     , (comment_unique_count / post_unique_count) AS comments_per_post_mean
+
+-- FROM (
+--     SELECT
+--         COUNT(*)       AS row_count
+--         , COUNT(DISTINCT post_id) AS post_unique_count
+--         , COUNT(DISTINCT comment_id) AS comment_unique_count
+--         , COUNT(DISTINCT subreddit_id) AS subreddit_unique_count
+
+--     FROM selected_comments
+-- );
+-- RESULT with:
+--    MIN_COMMENT_LEN NUMERIC = 10
+--    MAX_COMMENTS_PER_POST = 8
+--  row_count 	 post_unique_count 	 comment_unique_count 	 subreddit_unique_count 	 posts_per_subreddit_mean 	 comments_per_subreddit_mean 	 comments_per_post_mean
+--  3,337,142 	 652,663 	 3,337,142 	 4,073 	 160.2 	 819.3 	 5.1
+
+
+
+-- Check only comments for posts below comment-count threshold
+-- SELECT
+--     *
+--     , (post_unique_count / subreddit_unique_count) AS posts_per_subreddit_mean
+--     , (comment_unique_count / subreddit_unique_count) AS comments_per_subreddit_mean
+--     , (comment_unique_count / post_unique_count) AS comments_per_post_mean
+
+-- FROM (
+--     SELECT
+--         COUNT(*)       AS row_count
+--         , COUNT(DISTINCT post_id) AS post_unique_count
+--         , COUNT(DISTINCT comment_id) AS comment_unique_count
+--         , COUNT(DISTINCT subreddit_id) AS subreddit_unique_count
+
+--     FROM comments_from_posts_below_threshold
+-- );
+-- RESULT - coments from posts below threshold (8 comments per post)
+--  row_count 	 post_unique_count 	 comment_unique_count 	 subreddit_unique_count 	 posts_per_subreddit_mean 	 comments_per_subreddit_mean 	 comments_per_post_mean
+--  1,448,229 	 406,595 	 1,448,229 	 4,068 	 99.9 	 356.0 	 3.6
+
+
+-- Check comments for posts ABOVE comment-count threshold
+-- SELECT
+--     *
+--     , (post_unique_count / subreddit_unique_count) AS posts_per_subreddit_mean
+--     , (comment_unique_count / subreddit_unique_count) AS comments_per_subreddit_mean
+--     , (comment_unique_count / post_unique_count) AS comments_per_post_mean
+
+-- FROM (
+--     SELECT
+--         COUNT(*)       AS row_count
+--         , COUNT(DISTINCT post_id) AS post_unique_count
+--         , COUNT(DISTINCT comment_id) AS comment_unique_count
+--         , COUNT(DISTINCT subreddit_id) AS subreddit_unique_count
+
+--     FROM comments_from_posts_above_threshold
+--     WHERE 1=1
+--         AND comment_rank_by_post_id <= MAX_COMMENTS_PER_POST
+-- );
+-- RESULTS w/o filtering max comments per post
+--  row_count 	 post_unique_count 	 comment_unique_count 	 subreddit_unique_count 	 posts_per_subreddit_mean 	 comments_per_subreddit_mean 	 comments_per_post_mean
+--  7,900,663 	 246,068 	 7,900,663 	 2,373 	 103.7 	 3,329.4 	 32.1
+--  looks like we're only losing ~400k comments because they are too short (9.8 million - 1.4 - 7.9)
+
+-- RESULTS after limit to max comments per post. (mean per post should be max/limit, but we'll drop a few so maybe a bit lower than it)
+--  row_count 	 post_unique_count 	 comment_unique_count 	 subreddit_unique_count 	 posts_per_subreddit_mean 	 comments_per_subreddit_mean 	 comments_per_post_mean
+--  1,888,913 	 246,068 	 1,888,913 	 2,373 	 103.7 	 796.0 	 7.7
 
 
 -- Counts for AFTER dropping dupes from language detection table
