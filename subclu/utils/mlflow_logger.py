2b1540a41c9a0708024a4c8f892250a9b49fea62
David Bermejo <david.bermejo@reddit.com>
2021-10-12 17:27:21 -0500

    Reduce number of dask partitions when saving dfs
    by increasing the size of target-MB, we reduce the size of partitions... otherwise we could end up with thousands of little files

--- subclu/utils/mlflow_logger.py
+++ subclu/utils/mlflow_logger.py
@@ -535,13 +535,17 @@ def save_pd_df_to_parquet_in_chunks(
 
         if target_mb_size is None:
             if mem_usage_mb < 100:
-                target_mb_size = 30
+                target_mb_size = 50
             elif 100 <= mem_usage_mb < 1000:
-                target_mb_size = 40
+                target_mb_size = 100
             elif 1000 <= mem_usage_mb < 3000:
-                target_mb_size = 60
+                target_mb_size = 200
             else:
-                target_mb_size = 75
+                # We have some dfs that can take up over 156GB of RAM. So we need to increase the
+                #  target_mb size, otherwise we'll end up with thousands of tiny dfs. example:
+                # INFO | "  156,726.2 MB <- Memory usage"
+                # INFO | "   2,090  <- target Dask partitions      75.0 <- target MB partition size"
+                target_mb_size = 330
 
         n_dask_partitions = 1 + int(mem_usage_mb // target_mb_size)
 
