4382816d23400b625077a97c031b2d09cee498aa
David Bermejo <david.bermejo@reddit.com>
2022-03-30 00:55:00 -0500

    Add more logic to filter based on primary topic & rating

--- subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/other_eda/select_counterpart_candidates_for_germany.sql
+++ subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/other_eda/select_counterpart_candidates_for_germany.sql
@@ -1,14 +1,9 @@
--- Initial filter for subreddit counterparts
---  Note that we filter out more subreddits in a colab notebook
-
--- %%time
--- %%bigquery df_distances --project data-science-prod-218515
--- Get distances for geo-relevant subs
-
+-- Initial candidate list for subreddit counterparts
 DECLARE GEO_TARGET_COUNTRY_CODE_TARGET STRING DEFAULT "DE";
 DECLARE GEO_TARGET_LANGUAGE STRING DEFAULT "German";
 
-DECLARE MAX_COUNTERPARTS_TO_SHOW NUMERIC DEFAULT 9;
+DECLARE MIN_COUNTERPARTS_TO_SHOW NUMERIC DEFAULT 8;
+DECLARE MAX_COUNTERPARTS_TO_SHOW NUMERIC DEFAULT 15;
 DECLARE STANDARDIZED_COUNTRY_THRESHOLD NUMERIC DEFAULT 3.0;
 
 WITH
@@ -73,6 +68,10 @@ WITH
             , language_name_geo
             , language_percent_geo
             , language_rank_geo
+            , over_18 AS over_18_geo
+            , slo.allow_discovery AS allow_discovery_geo
+            , nt.rating_short AS rating_short_geo
+            , primary_topic AS primary_topic_geo
 
         FROM `reddit-employee-datasets.david_bermejo.subclu_v0041_subreddit_distances_c_top_100` AS d
             -- Get geo-relevance scores
@@ -80,19 +79,46 @@ WITH
                 ON d.subreddit_id_a = ga.subreddit_id
             LEFT JOIN subs_relevant_baseline AS gb
                 ON d.subreddit_id_b = gb.subreddit_id
+            LEFT JOIN `data-prod-165221.ds_v2_postgres_tables.subreddit_lookup` AS slo
+                ON d.subreddit_id_a = slo.subreddit_id
+            LEFT JOIN (
+                SELECT * FROM `data-prod-165221.cnc.shredded_crowdsource_topic_and_rating`
+                WHERE pt = (CURRENT_DATE() - 2)
+            ) AS nt
+                ON ga.subreddit_id = nt.subreddit_id
 
         -- Exclude subreddits that are geo-relevant to the country
-        WHERE gb.subreddit_id IS NULL
+        WHERE 1=1
+            AND slo.dt = (CURRENT_DATE() - 2)
+            AND gb.subreddit_id IS NULL
+
+            AND COALESCE(verdict, 'f') <> 'admin_removed'
+            AND COALESCE(is_spam, false) = false
+            AND COALESCE(is_deleted, false) = false
+            AND COALESCE(over_18, 'f') != 't'
     ),
     distance_lang_and_relevance_a_and_b AS (
         SELECT
-            a.* EXCEPT(language_name_geo, language_percent_geo, language_rank_geo)
+            a.* EXCEPT(
+                language_name_geo, language_percent_geo, language_rank_geo,
+                over_18_geo, rating_short_geo, primary_topic_geo, allow_discovery_geo
+            )
             , ROW_NUMBER() OVER (PARTITION BY subreddit_id_GEO ORDER BY cosine_similarity DESC) AS rank_geo_to_us
 
             , slo.subscribers AS subscribers_us
+
+            , rating_short_geo
+            , nt.rating_short AS rating_short_us
+            , primary_topic_geo
+            , primary_topic AS primary_topic_us
+            , allow_discovery_geo
+
             , language_name_geo, language_percent_geo, language_rank_geo
             , lan.language_name AS primary_language_name_us
             , lan.language_percent AS primary_language_percent_us
+            , over_18_geo
+            , over_18 AS over_18_us
+
         FROM distance_lang_and_relevance_a AS a
             LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_subreddit_relevance_beta_20220329` AS g
                 ON a.subreddit_id_us = g.subreddit_id
@@ -103,12 +129,25 @@ WITH
             -- get subscribers
             LEFT JOIN `data-prod-165221.ds_v2_postgres_tables.subreddit_lookup` AS slo
                 ON a.subreddit_id_us = slo.subreddit_id
+            LEFT JOIN (
+                SELECT * FROM `data-prod-165221.cnc.shredded_crowdsource_topic_and_rating`
+                WHERE pt = (CURRENT_DATE() - 2)
+            ) AS nt
+                ON a.subreddit_id_us = nt.subreddit_id
 
         WHERE 1=1
             AND slo.dt = (CURRENT_DATE() - 2)
-            AND slo.subscribers >= 9000
+            -- Exclude geo-subreddits that don't want to be discovered
+            AND COALESCE(allow_discovery_geo, '') != 'f'
 
             -- filters for US counterparts
+            AND slo.subscribers >= 9000
+            AND COALESCE(verdict, 'f') <> 'admin_removed'
+            AND COALESCE(is_spam, false) = false
+            AND COALESCE(is_deleted, false) = false
+            AND COALESCE(over_18, 'f') != 't'
+
+            -- more filters for US counterparts
             AND (
                 g.geo_country_code = 'US'
                 -- relevance filters
@@ -124,38 +163,42 @@ WITH
                     AND lan.language_percent >= 0.6
                 )
             )
+
+            -- Add filters for ratings & primary topics
+            -- AND COALESCE(rating_short_geo, '') = 'E'
+            -- AND COALESCE(nt.rating_short, '') = 'E'
+
+            AND COALESCE(primary_topic_geo, '') NOT IN (
+                'Place',
+                'Celebrity', 'Mature Themes and Adult Content',
+                'Sexual Orientation', 'Gender',
+                'Medical and Mental Health', 'Medical and Mental Health', 'Addiction Support',
+                'Politics', 'Military'
+            )
+            AND COALESCE(nt.primary_topic, '') NOT IN (
+                'Celebrity',
+                -- 'Mature Themes and Adult Content',
+                'Sexual Orientation', 'Gender',
+                'Medical and Mental Health', 'Medical and Mental Health', 'Addiction Support',
+                'Politics', 'Military'
+            )
+
     )
 
 
 SELECT
-    *
-FROM distance_lang_and_relevance_a_and_b
--- FROM distance_lang_and_relevance_a
--- FROM distance_for_country_subs
+    d.* EXCEPT(over_18_geo, over_18_us)
+    , sc.model_sort_order AS model_sort_order_geo
+FROM distance_lang_and_relevance_a_and_b AS d
+    -- join with cluster table so we can sort similar subreddits together
+    LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_subreddit_clusters_c_a` AS sc
+        ON d.subreddit_id_geo = sc.subreddit_id
 WHERE 1=1
     AND (
-        rank_geo_to_us <= (MAX_COUNTERPARTS_TO_SHOW + 5)
+        rank_geo_to_us <= MIN_COUNTERPARTS_TO_SHOW
         OR cosine_similarity >= 0.79
     )
     AND rank_geo_to_us <= MAX_COUNTERPARTS_TO_SHOW
 
-    -- testing filters
-    -- AND (
-    --     subreddit_name_geo IN (
-    --         '1fcnuernberg', 'bayernmunich', 'bundesliga', 'fcbayern', 'fussball',
-    --         'finanzen',
-    --         'germanrap', 'musik', 'rappers',
-    --         'buecher', 'harrypotterde',
-    --         -- 'filme',
-    --         -- 'heutelernteich', 'ich_iel', 'augenbleiche', 'fotografie',
-    --         'annode'
-
-    --         -- dead/no longer active
-    --         -- , 'lolde', 'kpopde', 'southparkde', 'diesimpsons',
-
-    --     )
-    -- )
-
-ORDER BY subreddit_name_geo, cosine_similarity DESC
-#  LIMIT 10000
+ORDER BY sc.model_sort_order DESC, subreddit_name_geo, subscribers_us DESC, cosine_similarity DESC
 ;
