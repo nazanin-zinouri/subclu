30ec3873ced9f0bdfa7bd6636df73269a488a30d
David Bermejo <david.bermejo@reddit.com>
2022-03-31 02:24:21 -0500

    Add fxns to get embeddings as DF

--- subclu/i18n_topic_model_batch/subclu2/get_embeddings/vectorize_text_tf.py
+++ subclu/i18n_topic_model_batch/subclu2/get_embeddings/vectorize_text_tf.py
@@ -26,7 +26,7 @@ from omegaconf import DictConfig
 
 from ..utils.eda import elapsed_time
 from ..utils.data_loaders_sql import LoadSubredditsSQL
-
+from ..utils.tqdm_logger import FileLogger, LogTQDM
 
 log = logging.getLogger(__name__)
 
@@ -84,8 +84,13 @@ class VectorizeText:
             model_name: str,
             data_loader_name: str,
             col_text_for_embeddings: str,
+            cols_index: Union[str, iter],
             data_loader_kwargs: dict = None,
             run_id: str = None,
+            tokenize_lowercase: bool = False,
+            batch_inference_rows: int = 2000,
+            limit_first_n_chars: int = 1000,
+            get_embeddings_verbose: bool = False,
             **kwargs
     ) -> None:
         """"""
@@ -95,6 +100,11 @@ class VectorizeText:
         self.model_name = model_name
         self.col_text_for_embeddings = col_text_for_embeddings
         self.data_loader_name = data_loader_name
+        self.tokenize_lowercase = tokenize_lowercase
+        self.batch_inference_rows = batch_inference_rows
+        self.limit_first_n_chars = limit_first_n_chars
+        self.get_embeddings_verbose = get_embeddings_verbose
+        self.cols_index = cols_index
 
         self.data_loader = DATA_LOADERS[data_loader_name](
             **data_loader_kwargs
@@ -107,24 +117,58 @@ class VectorizeText:
 
     def get_embeddings(self) -> pd.DataFrame:
         """Run process to get embeddings"""
+        t_start_vectorize = datetime.utcnow()
+        info(f"Start vectorize function")
 
         # TODO(djb): load model
         log.info(f"Lodaing model: {self.model_name}")
         model = self._load_model()
         log.info(f"Model loaded")
 
+        # TODO(djb): iterate over N files to get the text & get embeddings
+        #  will move away from SQL and back to GCS b/c files make it easier to run
+        #  in parallel
+
         # get text
         df_text = self.data_loader.get_as_dataframe()
 
         print(df_text[self.col_text_for_embeddings].head())
 
-        # TODO(djb): get embeddings
-
+        info(f"Vectorizing subreddit descriptions...")
+        t_start_subs_vect = datetime.utcnow()
+        df_vect = get_embeddings_as_df(
+            model=model,
+            df=df_text,
+            col_text=self.col_text_for_embeddings,
+            cols_index=self.cols_index,
+            lowercase_text=self.tokenize_lowercase,
+            batch_size=self.batch_inference_rows,
+            limit_first_n_chars=self.limit_first_n_chars,
+            verbose_init=self.get_embeddings_verbose,
+        )
+        total_time_subs_vect = elapsed_time(t_start_subs_vect, log_label='df_subs vectorizing', verbose=True)
+        # mlflow.log_metric('vectorizing_time_minutes_subreddit_meta',
+        #                   total_time_subs_vect / timedelta(minutes=1)
+        #                   )
+        log.info(f"{df_vect}")
+        print(df_vect.head())
         # TODO(djb): save embeddings
         #  for now, save straight to GCS, in the future shift to mlflow
         #  so we'd have to save to local first
 
-        return df_text[self.col_text_for_embeddings]
+        # save_df_and_log_to_mlflow(
+        #     df=df_vect_subs.reset_index(),
+        #     local_path=path_this_model,
+        #     name_for_metric_and_artifact_folder='df_vect_subreddits_description',
+        # )
+
+        # finish logging total time + end mlflow run
+        total_fxn_time = elapsed_time(start_time=t_start_vectorize, log_label='Total vectorize fxn', verbose=True)
+        # mlflow.log_metric('vectorizing_time_minutes_full_function',
+        #                   total_fxn_time / timedelta(minutes=1)
+        #                   )
+
+        return df_vect
 
     def _load_model(self):
         """Load model based on input
@@ -141,6 +185,145 @@ class VectorizeText:
             'use_multilingual_3': "https://tfhub.dev/google/universal-sentence-encoder-multilingual/3",
         }
         return hub.load(D_MODELS_TF_HUB[self.model_name])
+
+
+def get_embeddings_as_df(
+        model: callable,
+        df: pd.DataFrame,
+        col_text: str = 'text',
+        cols_index: Union[str, List[str]] = None,
+        col_embeddings_prefix: Optional[str] = 'embeddings',
+        lowercase_text: bool = False,
+        batch_size: int = 2000,
+        limit_first_n_chars: int = 1000,
+        limit_first_n_chars_retry: int = 600,
+        verbose: bool = True,
+        verbose_init: bool = False,
+) -> pd.DataFrame:
+    """Get output of TF model as a dataframe.
+    Besides batching we can get OOM (out of memory) errors if the text is too long,
+    so we'll be adding a limit to only embed the first N-characters in a column.
+
+    When called on a list a TF model runs in parallel, so use that instead of trying to
+    get model output on a dataframe (which would be sequential and slow).
+    For reference, on 5,400 sentences:
+    - ~2 seconds:   on list
+    - ~1 minute:    on text column df['text'].apply(model)
+
+    TODO(djb):  For each recursive call, use try/except!!
+      That way if one batch fails, the rest of the batches can proceed!
+    """
+    if cols_index == 'comment_default_':
+        cols_index = ['subreddit_name', 'subreddit_id', 'post_id', 'comment_id']
+    elif cols_index == 'post_default_':
+        cols_index = ['subreddit_name', 'subreddit_id', 'post_id']
+    elif cols_index == 'subreddit_default_':
+        cols_index = ['subreddit_name', 'subreddit_id']
+    else:
+        pass
+
+    if cols_index is not None:
+        index_output = df[cols_index]
+    else:
+        index_output = None
+
+    if batch_size is None:
+        iteration_chunks = None
+    elif batch_size >= len(df):
+        iteration_chunks = None
+    else:
+        iteration_chunks = range(1 + len(df) // batch_size)
+
+    if verbose_init:
+        info(f"cols_index: {cols_index}")
+        info(f"col_text: {col_text}")
+        info(f"lowercase_text: {lowercase_text}")
+        info(f"limit_first_n_chars: {limit_first_n_chars}")
+        info(f"limit_first_n_chars_retry: {limit_first_n_chars_retry}")
+
+    gc.collect()
+    if iteration_chunks is None:
+        if lowercase_text:
+            series_text = df[col_text].str.lower().str[:limit_first_n_chars]
+        else:
+            series_text = df[col_text].str[:limit_first_n_chars]
+
+        # In tf 2.3.4 it's faster to NOT use a list comprehension
+        #  These seem equivalent:
+        #   - np.array(model(series_text.to_list()))
+        #   - model(series_text.to_list()).numpy()
+        # df_vect = pd.DataFrame(
+        #     np.array([emb.numpy() for emb in model(series_text.to_list())])
+        # )
+        df_vect = pd.DataFrame(
+            model(series_text.to_list()).numpy()
+        )
+        if index_output is not None:
+            # Remember to reset the index of the output!
+            #   Because pandas will do an inner join based on index
+            df_vect = pd.concat(
+                [df_vect, index_output.reset_index(drop=True)],
+                axis=1,
+            ).set_index(cols_index)
+
+        if col_embeddings_prefix is not None:
+            # renaming can be expensive when we're calling the function recursively
+            # so only rename after all individual dfs are created
+            return df_vect.rename(
+                columns={c: f"{col_embeddings_prefix}_{c}" for c in df_vect.columns}
+            )
+        else:
+            return df_vect
+
+    else:
+        gc.collect()
+        # This seems like a good place for recursion(!)
+        # Renaming can be expensive when we're calling the function recursively
+        #   so only rename after all individual dfs are created
+        if verbose:
+            info(f"Getting embeddings in batches of size: {batch_size}")
+        l_df_embeddings = list()
+        for i in LogTQDM(
+                iteration_chunks, mininterval=11, ascii=True,  ncols=80,  # position=0, leave=True,
+                logger=log
+                ):
+            try:
+                l_df_embeddings.append(
+                    get_embeddings_as_df(
+                        model=model,
+                        df=df.iloc[i * batch_size:(i + 1) * batch_size],
+                        col_text=col_text,
+                        cols_index=cols_index,
+                        col_embeddings_prefix=None,
+                        lowercase_text=lowercase_text,
+                        batch_size=None,
+                        limit_first_n_chars=limit_first_n_chars,
+                    )
+                )
+                gc.collect()
+            except errors.ResourceExhaustedError as e:
+                logging.warning(f"\nResourceExhausted, lowering character limit\n{e}\n")
+                l_df_embeddings.append(
+                    get_embeddings_as_df(
+                        model=model,
+                        df=df.iloc[i * batch_size:(i + 1) * batch_size],
+                        col_text=col_text,
+                        cols_index=cols_index,
+                        col_embeddings_prefix=None,
+                        lowercase_text=lowercase_text,
+                        batch_size=None,
+                        limit_first_n_chars=limit_first_n_chars_retry,
+                    )
+                )
+                gc.collect()
+        if col_embeddings_prefix is not None:
+            df_vect = pd.concat(l_df_embeddings, axis=0, ignore_index=False)
+            return df_vect.rename(
+                columns={c: f"{col_embeddings_prefix}_{c}" for c in df_vect.columns}
+            )
+        else:
+            gc.collect()
+            return pd.concat(l_df_embeddings, axis=0, ignore_index=False)
 
 
 if __name__ == "__main__":
---
+++ subclu/i18n_topic_model_batch/subclu2/utils/tqdm_logger.py
@@ -0,0 +1,109 @@
+"""
+Utils to send tqdm info to log files - this class should log to both console AND files
+
+Inspired by open issue in github:
+https://github.com/tqdm/tqdm/issues/313
+"""
+from datetime import datetime
+import logging
+from pathlib import Path
+from typing import Union
+
+from tqdm import tqdm
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class LogTQDM(tqdm):
+    def __init__(
+            self,
+            *args,
+            logger: logging.Logger = None,
+            mininterval: float = .8,
+            ascii: bool = True,
+            ncols: int = 65,
+            position: int = 0,
+            leave: bool = True,
+            desc: str = 'progress: ',
+            # bar_format: str = '{desc}{percentage:3.0f}%{r_bar}',  # this only shows text w/o a progress bar
+            **kwargs):
+        if logger is not None:
+            self._logger = logger
+        else:
+            self._logger = LOGGER
+        super().__init__(
+            *args,
+            mininterval=mininterval,
+            ascii=ascii,
+            ncols=ncols,
+            position=position,
+            leave=leave,
+            desc=desc,
+            # bar_format=bar_format,
+            **kwargs
+        )
+
+    @property
+    def logger(self):
+        if self._logger is not None:
+            return self._logger
+        return LOGGER
+
+    def display(self, msg=None, pos=None):
+        if not self.n:
+            # skip progress bar before having processed anything
+            return
+        if not msg:
+            msg = self.__str__()
+        self.logger.info('%s', msg)
+
+
+class FileLogger:
+    def __init__(
+            self,
+            logs_path: Union[str, Path],
+            log_name: str = 'log',
+            log_level = logging.INFO
+    ):
+        """"""
+        self.logs_path = Path(logs_path)
+        self.log_name = log_name
+        self.log_level = log_level
+
+        self.f_log_file = str(
+            self.logs_path /
+            f"{datetime.utcnow().strftime('%Y-%m-%d_%H-%M-%S')}_{self.log_name}.log"
+        )
+        self.fileHandler = None
+
+    def init_file_log(self) -> None:
+        """Create a file & FileHandler to log data"""
+        logger = logging.getLogger()
+        Path.mkdir(self.logs_path, parents=True, exist_ok=True)
+
+        self.fileHandler = logging.FileHandler(self.f_log_file)
+        self.fileHandler.setLevel(self.log_level)
+
+        formatter = logging.Formatter(
+            '%(asctime)s | %(levelname)s | "%(message)s"',
+            '%Y-%m-%d %H:%M:%S'
+        )
+        self.fileHandler.setFormatter(formatter)
+        logger.addHandler(self.fileHandler)
+
+    def remove_file_logger(self) -> None:
+        """After completing job, remove logging handler to prevent
+        info from other jobs getting logged to the same log file
+        """
+        if self.fileHandler is not None:
+            logger = logging.getLogger()
+            try:
+                logger.removeHandler(self.fileHandler)
+            except Exception as e:
+                logging.warning(f"Can't remove logger\n{e}")
+
+
+#
+# ~ fin
+#
