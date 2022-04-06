8e134d3b8062567fec868e539761441be246b27b
David Bermejo <david.bermejo@reddit.com>
2022-04-06 12:08:09 -0500

    Update SQL queries to check kubeflow creation tables

--- subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/checks_for_kubeflow_tables/misc_checks.sql
+++ subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/checks_for_kubeflow_tables/misc_checks.sql
@@ -10,8 +10,25 @@ ORDER BY country_code ASC
 
 -- Check relevant countries for a specific subreddit
 SELECT *
-FROM `reddit-relevance.tmp.subclu_subreddit_geo_relevance_standardized_20220323`
+FROM `reddit-relevance.tmp.subclu_subreddit_geo_relevance_standardized_202200404`
 WHERE subreddit_name IN ('futbol')
 
 ORDER BY relevance_combined_score DESC
 ;
+
+-- ============
+-- Check how many subs qualify b/c they're i18n core/ambassador subs
+-- ===
+SELECT *
+FROM `reddit-relevance.tmp.subclu_subreddit_geo_selected_20220404`
+WHERE 1=1
+    AND i18n_type IS NOT NULL
+    OR  i18n_type_2 IS NOT NULL
+LIMIT 1000;
+
+SELECT *
+FROM `reddit-relevance.tmp.subclu_subreddit_candidates_20220404`
+WHERE 1=1
+    AND i18n_type IS NOT NULL
+    OR  i18n_type_2 IS NOT NULL
+LIMIT 1000;
