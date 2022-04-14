332425c3fb2973def03fda8e51d8b67f74bab93a
David Bermejo <david.bermejo@reddit.com>
2022-04-13 20:49:48 -0500

    Update GCS class to cache parquet files locally

--- subclu/i18n_topic_model_batch/subclu2/utils/data_loaders_gcs.py
+++ subclu/i18n_topic_model_batch/subclu2/utils/data_loaders_gcs.py
@@ -69,11 +69,29 @@ class LoadSubredditsGCS:
         else:
             self.read_fxn = read_fxn
 
+    def _local_cache(self) -> None:
+        """Cache files to read from GCS in local"""
+        # load files
+        d_local_cache = download_files_in_parallel(
+            bucket_name=self.bucket_name,
+            gcs_folder_path=self.gcs_path,
+            local_path_root=self.local_path_root,
+            n_sample_files=self.n_sample_files,
+            verbose=self.verbose,
+        )
+
+        # get names for the individual parquet files to read
+        self.path_local_folder = d_local_cache['path_local_folder']
+        self.local_files_ = d_local_cache['files_downloaded']
+        self.local_parquet_files_ = d_local_cache['parquet_files_downloaded']
+
+
 
 def download_blob(
         blob: Blob,
         local_file_name: str,
 ) -> None:
+    """Use this helper function to download multiple files in parallel"""
     blob.download_to_filename(local_file_name)
 
 
@@ -81,6 +99,10 @@ def download_files_in_parallel(
         bucket_name: str,
         gcs_folder_path: str,
         local_path_root: str,
+        n_sample_files: int = None,
+        n_files_slice_start: int = None,
+        n_files_slice_end: int = None,
+        verbose: bool = False,
 ) -> dict:
     t_start_ = datetime.utcnow()
     storage_client = storage.Client()
@@ -91,40 +113,57 @@ def download_files_in_parallel(
     artifact_folder = gcs_folder_path.split('/')[-1]
     info(f"  Local folder to download artifact(s):\n  {path_local_folder}")
     Path.mkdir(path_local_folder, exist_ok=True, parents=True)
-    d_output: {
+    d_output = {
         'path_local_folder': path_local_folder
     }
 
-    pool = futures.ThreadPoolExecutor()
-    downloads = []
-
-    l_files_to_check = list(bucket.list_blobs(prefix=gcs_folder_path))
     l_files_downloaded = []
+    l_parquet_files_downloaded = []
 
-    for blob_ in l_files_to_check:
-        # Skip files that aren't in the same folder as the expected (input) folder
-        parent_folder = blob_.name.split('/')[-2]
-        if artifact_folder != parent_folder:
-            continue
+    l_files_to_check = list(bucket.list_blobs(prefix=gcs_folder_path))[:n_sample_files]
+    # if n_comment_files_slice_end is not None:
+    #     if n_comment_files_slice_start is None:
+    #         n_comment_files_slice_start = 0
+    #     # make new copy of files to process so that when slicing the time estimates from tqdm
+    #     #  are useful/accurate
+    #     l_comment_files_to_process = (
+    #         l_comment_files_to_process[n_comment_files_slice_start:n_comment_files_slice_end]
+    #     )
+    # total_comms_file_count = len(l_comment_files_to_process)
 
-        f_name = (
-                path_local_folder /
-                f"{blob_.name.split('/')[-1].strip()}"
-        )
-        l_files_downloaded.append(f_name)
-        if f_name.exists():
-            pass
-            # info(f"  {f_name.name} <- File already exists, not downloading")
-        else:
-            download = pool.submit(blob_, f_name)
-            downloads.append(download)
+    info(f"  Files to check: {len(l_files_to_check)}")
+    n_cached_files = 0
+
+    downloads = []
+    with futures.ThreadPoolExecutor(max_workers=4) as executor:
+        for blob_ in l_files_to_check:
+            # Skip files that aren't in the same folder as the expected immediate folder
+            parent_folder = blob_.name.split('/')[-2]
+            if artifact_folder != parent_folder:
+                continue
+
+            f_name = (
+                    path_local_folder /
+                    f"{blob_.name.split('/')[-1].strip()}"
+            )
+            l_files_downloaded.append(f_name)
+            if '.parquet' == f_name.suffix:
+                l_parquet_files_downloaded.append(f_name)
+            if f_name.exists():
+                n_cached_files += 1
+                if verbose:
+                    info(f"    {f_name.name} <- File already exists, not downloading")
+            else:
+                downloads.append(
+                    executor.submit(download_blob, blob_, f_name)
+                )
 
     futures.wait(downloads)
     d_output['files_downloaded'] = l_files_downloaded
+    d_output['parquet_files_downloaded'] = l_parquet_files_downloaded
 
     t_total_ = datetime.utcnow() - t_start_
-    logger.info(
-        "Downloading files took %.2f minutes (%.2f seconds) ---", t_total_ / 60, t_total_
-    )
+    info(f"  Files already cached: {n_cached_files}")
+    logger.info(f"Downloading files took {t_total_}")
 
     return d_output
