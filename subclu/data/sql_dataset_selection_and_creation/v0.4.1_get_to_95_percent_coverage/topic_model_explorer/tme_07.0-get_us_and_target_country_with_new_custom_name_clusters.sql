1188e2a0f44322f63ae7e81306d57f0be77d48e4
David Bermejo <david.bermejo@reddit.com>
2022-05-12 20:27:30 -0500

    Remove percentage columns that will be dynamic in Mode
    We'll calculate percentages in Mode so that the calculations are dynamic

--- subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/topic_model_explorer/tme_07.0-get_us_and_target_country_with_new_custom_name_clusters.sql
+++ subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/topic_model_explorer/tme_07.0-get_us_and_target_country_with_new_custom_name_clusters.sql
@@ -4,7 +4,7 @@
 -- ===============
 -- Limits for geo-relevance
 -- ===
-DECLARE GEO_TARGET_COUNTRY_NAME STRING DEFAULT "Germany";
+DECLARE GEO_TARGET_COUNTRY_CODE STRING DEFAULT '{{ geo_country_code }}';
 
 DECLARE MIN_USERS_PERCENT_BY_SUBREDDIT_L28 NUMERIC DEFAULT 0.20; -- default is 0.14 (14%)
 DECLARE MIN_USERS_PERCENT_BY_COUNTRY_STANDARDIZED NUMERIC DEFAULT 3.0; -- default is 3.0
@@ -22,22 +22,8 @@ DECLARE TOP_N_WORDS_FROM_BM25 NUMERIC DEFAULT 12;
 
 
 WITH
--- Get pre-computed TF-IDF & BM25 table
-tf_idf_single_row_per_cluster AS (
-    SELECT
-        tf.k_0100_label_name
-        , STRING_AGG(ngram, ', ') AS top_keywords
-    FROM `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_tfidf_bm25` AS tf
-    WHERE 1=1
-        AND (
-            ngram_rank_bm25 <= TOP_N_WORDS_FROM_BM25
-            OR ngram_rank_tfidf <= TOP_N_WORDS_FROM_TFIDF
-        )
-    GROUP BY 1
-)
-
 -- Define US subreddits
-, subreddits_relevant_us AS (
+subreddits_relevant_us AS (
     SELECT
         sa.* EXCEPT(users_l7_rank_100, users_l7_rank_400)
         , m.k_0100_label_name
@@ -78,32 +64,21 @@ tf_idf_single_row_per_cluster AS (
     SELECT
         k_0100_label_name
         , COUNT(DISTINCT subreddit_id) AS subreddits_in_cluster_count_us
-        , (
-            COUNT(DISTINCT subreddit_id) * 100.0 /
-            SUM(COUNT(DISTINCT subreddit_id)) OVER ()
-        ) AS subreddits_in_cluster_pct_us
+        -- Don't calculate percentages of total here, do it in MODE b/c we can do it dynamically there
+        -- , (
+        --     COUNT(DISTINCT subreddit_id) * 1.0 /
+        --     SUM(COUNT(DISTINCT subreddit_id)) OVER ()
+        -- ) AS subreddits_in_cluster_pct_us
 
         , SUM(users_l7) AS users_l7_cluster_sum_us
-        , (
-            SUM(users_l7) * 100.0 /
-            SUM(SUM(users_l7)) OVER ()
-        ) AS users_l7_cluster_pct_of_total_us
 
         , SUM(seo_users_l7) AS seo_users_l7_cluster_sum_us
         , (
-            SUM(seo_users_l7) * 100.0 /
+            SUM(seo_users_l7) * 1.0 /
             SUM(users_l7)
         ) AS seo_users_l7_pct_of_cluster_us
-        , (
-            SUM(seo_users_l7) * 100.0 /
-            SUM(SUM(seo_users_l7)) OVER ()
-        ) AS seo_users_l7_cluster_pct_of_total_us
 
         , SUM(posts_l7) AS posts_l7_cluster_sum_us
-        , (
-            SUM(posts_l7) * 100.0 /
-            SUM(SUM(posts_l7)) OVER ()
-        ) AS posts_l7_cluster_pct_of_total_us
 
     FROM subreddits_relevant_us
     GROUP BY 1
@@ -139,7 +114,7 @@ tf_idf_single_row_per_cluster AS (
             OR users_percent_by_subreddit_l28 >= 0.14
             OR users_percent_by_country_standardized >= 3.0
         )
-        AND country_name = GEO_TARGET_COUNTRY_NAME
+        AND geo_country_code = GEO_TARGET_COUNTRY_CODE
 
         -- Optional: Exclude NSFW subreddits in mostly SFW clusters
         AND COALESCE(m.use_for_global_tfidf, true) != false
@@ -160,32 +135,16 @@ tf_idf_single_row_per_cluster AS (
     SELECT
         k_0100_label_name
         , COUNT(DISTINCT subreddit_id) AS subreddits_in_cluster_count_geo
-        , (
-            COUNT(DISTINCT subreddit_id) * 100.0 /
-            SUM(COUNT(DISTINCT subreddit_id)) OVER ()
-        ) AS subreddits_in_cluster_pct_geo
 
         , SUM(users_l7) AS users_l7_cluster_sum_geo
-        , (
-            SUM(users_l7) * 100.0 /
-            SUM(SUM(users_l7)) OVER ()
-        ) AS users_l7_cluster_pct_of_total_geo
 
         , SUM(seo_users_l7) AS seo_users_l7_cluster_sum_geo
         , (
-            SUM(seo_users_l7) * 100.0 /
+            SUM(seo_users_l7) * 1.0 /
             SUM(users_l7)
         ) AS seo_users_l7_pct_of_cluster_geo
-        , (
-            SUM(seo_users_l7) * 100.0 /
-            SUM(SUM(seo_users_l7)) OVER ()
-        ) AS seo_users_l7_cluster_pct_of_total_geo
 
         , SUM(posts_l7) AS posts_l7_cluster_sum_geo
-        , (
-            SUM(posts_l7) * 100.0 /
-            SUM(SUM(posts_l7)) OVER ()
-        ) AS posts_l7_cluster_pct_of_total_geo
 
     FROM subreddits_relevant_geo
     GROUP BY 1
@@ -209,51 +168,64 @@ tf_idf_single_row_per_cluster AS (
         , us.top_subreddits_us
         , geo.top_subreddits_geo
         , subreddits_in_cluster_count_us, subreddits_in_cluster_count_geo
-        , subreddits_in_cluster_pct_us, subreddits_in_cluster_pct_geo
 
-        , (
-            COALESCE(geo.subreddits_in_cluster_pct_geo, 0.0) -
-            COALESCE(us.subreddits_in_cluster_pct_us, 0.0)
-        ) AS subreddits_in_cluster_pct_diff_geo_from_us
-        , (
-            COALESCE(geo.users_l7_cluster_pct_of_total_geo, 0.0) -
-            COALESCE(us.users_l7_cluster_pct_of_total_us, 0.0)
-        ) AS users_l7_pct_diff_geo_from_us
-        , (
-            COALESCE(geo.seo_users_l7_cluster_pct_of_total_geo, 0.0) -
-            COALESCE(us.seo_users_l7_cluster_pct_of_total_us, 0.0)
-        ) AS seo_users_l7_pct_diff_geo_from_us
-        , (
-            COALESCE(geo.posts_l7_cluster_pct_of_total_geo, 0.0) -
-            COALESCE(us.posts_l7_cluster_pct_of_total_us, 0.0)
-        ) AS posts_l7_pct_diff_geo_from_us
+        -- Calculate diff in MODE because we want to apply dynamic filters
+        -- , (
+        --     COALESCE(geo.subreddits_in_cluster_pct_geo, 0.0) -
+        --     COALESCE(us.subreddits_in_cluster_pct_us, 0.0)
+        -- ) AS subreddits_in_cluster_pct_diff_geo_from_us
 
-        , us.* EXCEPT(k_0100_label_name, top_subreddits_us, subreddits_in_cluster_count_us, subreddits_in_cluster_pct_us)
-        , geo.* EXCEPT(k_0100_label_name, top_subreddits_geo, subreddits_in_cluster_count_geo, subreddits_in_cluster_pct_geo)
+        , us.* EXCEPT(k_0100_label_name, top_subreddits_us, subreddits_in_cluster_count_us)
+        , geo.* EXCEPT(k_0100_label_name, top_subreddits_geo, subreddits_in_cluster_count_geo)
     FROM top_subreddits_and_agg_per_cluster_us AS us
         FULL OUTER JOIN top_subreddits_and_agg_per_cluster_geo AS geo
             ON us.k_0100_label_name = geo.k_0100_label_name
 )
+-- Get pre-computed TF-IDF & BM25 table
+-- , tf_idf_single_row_per_cluster AS (
+--     SELECT
+--         tf.k_0100_label_name
+--         , STRING_AGG(ngram, ', ') AS top_keywords
+--     FROM `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_tfidf_bm25` AS tf
+--     WHERE 1=1
+--         AND (
+--             ngram_rank_bm25 <= TOP_N_WORDS_FROM_BM25
+--             OR ngram_rank_tfidf <= TOP_N_WORDS_FROM_TFIDF
+--         )
+--     GROUP BY 1
+-- )
 
 
 -- Get 1 row per cluster, final output
 SELECT
-    top_keywords AS cluster_top_keywords
-    , tf.k_0100_label_name
+    m.k_0100_label_name AS cluster_name
 
     , cc.* EXCEPT(k_0100_label_name)
 
-    , pt1.top_topic AS cluster_top_topic
-    , pt1.top_topic_percent AS cluster_top_topic_percent
-    , pt1.cluster_primary_topics AS cluster_primary_topics_and_pct
+    -- Exclude top keywords to speed up query
+    -- , top_keywords AS cluster_top_keywords
 
-FROM tf_idf_single_row_per_cluster tf
+    -- Topic summary. Exclude to speed up query
+    -- , pt1.top_topic AS cluster_top_topic
+    -- , pt1.top_topic_percent AS cluster_top_topic_percent
+    -- , pt1.cluster_primary_topics AS cluster_primary_topics_and_pct
+
+-- Start with the full cluster lest on the left because that way we guarantee we'll have infor for all clusters
+--  Even if they're not in target country
+FROM (
+    SELECT DISTINCT k_0100_label_name
+    FROM `reddit-employee-datasets.david_bermejo.subclu_v0041_subreddit_clusters_c_manual_names`
+) AS m
     LEFT JOIN cluster_compare_us_v_geo AS cc
-        ON tf.k_0100_label_name = cc.k_0100_label_name
+        ON m.k_0100_label_name = cc.k_0100_label_name
+
+    -- Remove keywords for now b/c they take too much time. Maybe run as separate query
+    -- LEFT JOIN tf_idf_single_row_per_cluster AS tf
+    --     ON tf.k_0100_label_name = cc.k_0100_label_name
 
     -- Topic aggregates from parent cluster ID
-    LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_top_topics_manual` AS pt1
-        ON tf.k_0100_label_name = pt1.k_0100_label_name
+    -- LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_top_topics_manual` AS pt1
+    --     ON cc.k_0100_label_name = pt1.k_0100_label_name
 
     -- Rating aggregates: don't use for now b/c they're not at the right grain
     -- LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_top_ratings` AS pr1
