9647f75edcd75090e872cb9d89d59157e732eb40
David Bermejo <david.bermejo@reddit.com>
2022-05-12 20:05:08 -0500

    Update query for mode dashboard

--- subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/topic_model_explorer/tme_07.0-get_us_and_target_country_with_new_custom_name_clusters.sql
+++ subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/topic_model_explorer/tme_07.0-get_us_and_target_country_with_new_custom_name_clusters.sql
@@ -4,7 +4,9 @@
 -- ===============
 -- Limits for geo-relevance
 -- ===
-DECLARE MIN_USERS_PERCENT_BY_SUBREDDIT_L28 NUMERIC DEFAULT 0.14; -- default is 0.14 (14%)
+DECLARE GEO_TARGET_COUNTRY_NAME STRING DEFAULT "Germany";
+
+DECLARE MIN_USERS_PERCENT_BY_SUBREDDIT_L28 NUMERIC DEFAULT 0.20; -- default is 0.14 (14%)
 DECLARE MIN_USERS_PERCENT_BY_COUNTRY_STANDARDIZED NUMERIC DEFAULT 3.0; -- default is 3.0
 
 -- Variables for subreddits to show
@@ -26,158 +28,236 @@ tf_idf_single_row_per_cluster AS (
         tf.k_0100_label_name
         , STRING_AGG(ngram, ', ') AS top_keywords
     FROM `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_tfidf_bm25` AS tf
-        WHERE 1=1
-            AND (
-                ngram_rank_bm25 <= 12       -- TOP_N_WORDS_FROM_BM25
-                OR ngram_rank_tfidf <= 12   -- TOP_N_WORDS_FROM_TFIDF
-            )
-
+    WHERE 1=1
+        AND (
+            ngram_rank_bm25 <= TOP_N_WORDS_FROM_BM25
+            OR ngram_rank_tfidf <= TOP_N_WORDS_FROM_TFIDF
+        )
     GROUP BY 1
 )
 
-
 -- Define US subreddits
-, subreddits_us_relevant AS (
+, subreddits_relevant_us AS (
     SELECT
         sa.* EXCEPT(users_l7_rank_100, users_l7_rank_400)
-        -- TODO(djb): rank over new manual cluster name!
-        , ROW_NUMBER() OVER (PARTITION BY k_0100_label ORDER BY users_l7 DESC, users_l28 DESC) as users_l7_rank_100
+        , m.k_0100_label_name
+        -- , m.k_0400_label_name
+        , ROW_NUMBER() OVER (PARTITION BY m.k_0100_label_name ORDER BY users_l7 DESC, users_l28 DESC) as users_l7_rank_top_topic
+        -- Output table only groups by top level, so we don't need lower level rank here
+        -- , ROW_NUMBER() OVER (PARTITION BY m.k_0400_label_name ORDER BY users_l7 DESC, users_l28 DESC) as users_l7_rank_subtopic
     FROM `reddit-employee-datasets.david_bermejo.subclu_v0041_subreddit_activity` AS sa
         INNER JOIN `reddit-employee-datasets.david_bermejo.subclu_subreddit_relevance_beta_20220502` AS rel
             ON sa.subreddit_id = rel.subreddit_id
-        -- TODO(djb): Merge with the new manual labels table so that we can get the rank by new cluster name
+        -- Merge with the new manual labels table so that we can get the rank by new cluster name
+        LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_subreddit_clusters_c_manual_names` AS m
+            ON sa.k_0400_label = m.k_0400_label
     WHERE 1=1
         AND (
             -- The US is so big that we're only taking into account the geo relevance default score
             geo_relevance_default = TRUE
         )
         AND geo_country_code = 'US'
+        -- Exclude NSFW subreddits in mostly SFW clusters
+        AND COALESCE(m.use_for_global_tfidf, true) != false
 )
 
-, top_subreddits_us_per_cluster AS (
+, top_subreddits_per_cluster_us AS (
+    -- Here we get the names of the top N subreddits in the country
     SELECT
-        k_0100_label
-        , STRING_AGG(subreddit_name, ', ') AS top_subreddits
-    FROM (
-      -- TODO(djb): Change it so this only pulls from US subreddits
-        SELECT
-            k_0100_label
-            , subreddit_name
-            , users_l7_rank_400
-            , users_l7_rank_100
-            FROM `reddit-employee-datasets.david_bermejo.subclu_v0041_subreddit_activity`
-            ORDER BY users_l7_rank_100
-        )
+        k_0100_label_name
+        , STRING_AGG(subreddit_name, ', ' ORDER BY users_l7_rank_top_topic) AS top_subreddits_us
+    FROM subreddits_relevant_us
     WHERE 1=1
-        AND users_l7_rank_100 <= N_US_SUBREDDITS_IN_AGG_SUMMARY
+        AND users_l7_rank_top_topic <= N_US_SUBREDDITS_IN_AGG_SUMMARY
     GROUP BY 1
 )
 
+, subreddits_agg_stats_per_cluster_us AS (
+    -- Here we get stats for ALL subreddits in a country
+    -- Get counts AND percentages
+    SELECT
+        k_0100_label_name
+        , COUNT(DISTINCT subreddit_id) AS subreddits_in_cluster_count_us
+        , (
+            COUNT(DISTINCT subreddit_id) * 100.0 /
+            SUM(COUNT(DISTINCT subreddit_id)) OVER ()
+        ) AS subreddits_in_cluster_pct_us
 
-SELECT COUNT(*)
-FROM subreddits_us_relevant
-;
+        , SUM(users_l7) AS users_l7_cluster_sum_us
+        , (
+            SUM(users_l7) * 100.0 /
+            SUM(SUM(users_l7)) OVER ()
+        ) AS users_l7_cluster_pct_of_total_us
 
--- -- Define Target-country subreddits
-, subreddits_in_cluster_and_country AS (
+        , SUM(seo_users_l7) AS seo_users_l7_cluster_sum_us
+        , (
+            SUM(seo_users_l7) * 100.0 /
+            SUM(users_l7)
+        ) AS seo_users_l7_pct_of_cluster_us
+        , (
+            SUM(seo_users_l7) * 100.0 /
+            SUM(SUM(seo_users_l7)) OVER ()
+        ) AS seo_users_l7_cluster_pct_of_total_us
+
+        , SUM(posts_l7) AS posts_l7_cluster_sum_us
+        , (
+            SUM(posts_l7) * 100.0 /
+            SUM(SUM(posts_l7)) OVER ()
+        ) AS posts_l7_cluster_pct_of_total_us
+
+    FROM subreddits_relevant_us
+    GROUP BY 1
+)
+-- Join the top US subs and cluster aggregates into a single table
+, top_subreddits_and_agg_per_cluster_us AS (
+    SELECT
+        st.top_subreddits_us
+        , sa.*
+    FROM subreddits_agg_stats_per_cluster_us AS sa
+        LEFT JOIN top_subreddits_per_cluster_us AS st
+            ON sa.k_0100_label_name = st.k_0100_label_name
+    -- ORDER BY 1
+)
+
+-- Define Target-country subreddits
+, subreddits_relevant_geo AS (
     SELECT
         sa.* EXCEPT(users_l7_rank_100, users_l7_rank_400)
-        , ROW_NUMBER() OVER (PARTITION BY k_0100_label ORDER BY users_l7 DESC, users_l28 DESC) as users_l7_rank_100
+        , m.k_0100_label_name
+        , ROW_NUMBER() OVER (PARTITION BY m.k_0100_label_name ORDER BY users_l7 DESC, users_l28 DESC) as users_l7_rank_top_topic
+
     FROM `reddit-employee-datasets.david_bermejo.subclu_v0041_subreddit_activity` AS sa
         INNER JOIN `reddit-employee-datasets.david_bermejo.subclu_subreddit_relevance_beta_20220502` AS rel
             ON sa.subreddit_id = rel.subreddit_id
+        -- Merge with the new manual labels table so that we can get the rank by new cluster name
+        LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_subreddit_clusters_c_manual_names` AS m
+            ON sa.k_0400_label = m.k_0400_label
     WHERE 1=1
         AND (
+            -- For the target country include more subreddits that are relevant with other scores
             geo_relevance_default = TRUE
-            OR relevance_percent_by_subreddit = TRUE
+            OR users_percent_by_subreddit_l28 >= 0.14
             OR users_percent_by_country_standardized >= 3.0
         )
-        AND geo_country_code IN ('DE')
-)
-, subreddits_in_country_cluster_agg AS (
-    SELECT
-        k_0100_label
-        , COUNT(DISTINCT subreddit_id) AS subreddits_in_cluster_germany
-        , SUM(users_l7) AS users_l7_cluster_total_germany
-    FROM subreddits_in_cluster_and_country
-    GROUP BY 1
-)
-, top_subreddits_per_cluster_country AS (
-    SELECT
-        sc.k_0100_label
-        , STRING_AGG(subreddit_name, ', ') AS top_subreddits_germany
+        AND country_name = GEO_TARGET_COUNTRY_NAME
 
-    FROM (
-        SELECT
-            k_0100_label
-            , subreddit_name
-            , users_l7_rank_100
-        FROM subreddits_in_cluster_and_country
-        ORDER BY k_0100_label ASC, users_l7_rank_100
-     ) AS sc
+        -- Optional: Exclude NSFW subreddits in mostly SFW clusters
+        AND COALESCE(m.use_for_global_tfidf, true) != false
+)
+, top_subreddits_per_cluster_geo AS (
+    -- Here we get the names of the top N subreddits in the country
+    SELECT
+        k_0100_label_name
+        , STRING_AGG(subreddit_name, ', ' ORDER BY users_l7_rank_top_topic) AS top_subreddits_geo
+    FROM subreddits_relevant_geo
     WHERE 1=1
-        AND users_l7_rank_100 <= 20
+        AND users_l7_rank_top_topic <= N_GEO_SUBREDDITS_IN_AGG_SUMMARY
     GROUP BY 1
-
 )
-, top_subs_and_agg_germany AS (
+, subreddits_agg_stats_per_cluster_geo AS (
+    -- Here we get stats for ALL subreddits in a country
+    -- Get counts AND percentages
     SELECT
-        sa.*
-        , sc.top_subreddits_germany
-    FROM subreddits_in_country_cluster_agg AS sa
-        LEFT JOIN top_subreddits_per_cluster_country AS sc
-            ON sa.k_0100_label = sc.k_0100_label
-    ORDER BY 1
+        k_0100_label_name
+        , COUNT(DISTINCT subreddit_id) AS subreddits_in_cluster_count_geo
+        , (
+            COUNT(DISTINCT subreddit_id) * 100.0 /
+            SUM(COUNT(DISTINCT subreddit_id)) OVER ()
+        ) AS subreddits_in_cluster_pct_geo
+
+        , SUM(users_l7) AS users_l7_cluster_sum_geo
+        , (
+            SUM(users_l7) * 100.0 /
+            SUM(SUM(users_l7)) OVER ()
+        ) AS users_l7_cluster_pct_of_total_geo
+
+        , SUM(seo_users_l7) AS seo_users_l7_cluster_sum_geo
+        , (
+            SUM(seo_users_l7) * 100.0 /
+            SUM(users_l7)
+        ) AS seo_users_l7_pct_of_cluster_geo
+        , (
+            SUM(seo_users_l7) * 100.0 /
+            SUM(SUM(seo_users_l7)) OVER ()
+        ) AS seo_users_l7_cluster_pct_of_total_geo
+
+        , SUM(posts_l7) AS posts_l7_cluster_sum_geo
+        , (
+            SUM(posts_l7) * 100.0 /
+            SUM(SUM(posts_l7)) OVER ()
+        ) AS posts_l7_cluster_pct_of_total_geo
+
+    FROM subreddits_relevant_geo
+    GROUP BY 1
+)
+-- Join the top GEO subs and cluster aggregates into a single table
+, top_subreddits_and_agg_per_cluster_geo AS (
+    SELECT
+        st.top_subreddits_geo
+        , sa.*
+    FROM subreddits_agg_stats_per_cluster_geo AS sa
+        LEFT JOIN top_subreddits_per_cluster_geo AS st
+            ON sa.k_0100_label_name = st.k_0100_label_name
+    -- ORDER BY 1
+)
+
+-- Merge US & Geo + Calculate over & under-index per cluster
+, cluster_compare_us_v_geo AS (
+    SELECT
+        -- We need coalesce b/c not all clusters are guaranteed for all countries
+        COALESCE(us.k_0100_label_name, geo.k_0100_label_name) AS k_0100_label_name
+        , us.top_subreddits_us
+        , geo.top_subreddits_geo
+        , subreddits_in_cluster_count_us, subreddits_in_cluster_count_geo
+        , subreddits_in_cluster_pct_us, subreddits_in_cluster_pct_geo
+
+        , (
+            COALESCE(geo.subreddits_in_cluster_pct_geo, 0.0) -
+            COALESCE(us.subreddits_in_cluster_pct_us, 0.0)
+        ) AS subreddits_in_cluster_pct_diff_geo_from_us
+        , (
+            COALESCE(geo.users_l7_cluster_pct_of_total_geo, 0.0) -
+            COALESCE(us.users_l7_cluster_pct_of_total_us, 0.0)
+        ) AS users_l7_pct_diff_geo_from_us
+        , (
+            COALESCE(geo.seo_users_l7_cluster_pct_of_total_geo, 0.0) -
+            COALESCE(us.seo_users_l7_cluster_pct_of_total_us, 0.0)
+        ) AS seo_users_l7_pct_diff_geo_from_us
+        , (
+            COALESCE(geo.posts_l7_cluster_pct_of_total_geo, 0.0) -
+            COALESCE(us.posts_l7_cluster_pct_of_total_us, 0.0)
+        ) AS posts_l7_pct_diff_geo_from_us
+
+        , us.* EXCEPT(k_0100_label_name, top_subreddits_us, subreddits_in_cluster_count_us, subreddits_in_cluster_pct_us)
+        , geo.* EXCEPT(k_0100_label_name, top_subreddits_geo, subreddits_in_cluster_count_geo, subreddits_in_cluster_pct_geo)
+    FROM top_subreddits_and_agg_per_cluster_us AS us
+        FULL OUTER JOIN top_subreddits_and_agg_per_cluster_geo AS geo
+            ON us.k_0100_label_name = geo.k_0100_label_name
 )
 
 
--- Get 1 row per cluster
+-- Get 1 row per cluster, final output
 SELECT
-    tf.cluster_id AS k_0100_label
+    top_keywords AS cluster_top_keywords
+    , tf.k_0100_label_name
 
-    -- , tf.cluster_id AS k_0400_label
-    -- , pt.subreddits_in_cluster_count AS subreddits_in_cluster_count_400
+    , cc.* EXCEPT(k_0100_label_name)
 
-    , pt1.top_topic AS top_topic_100
-    , pt1.top_topic_percent AS top_topic_percent_100
+    , pt1.top_topic AS cluster_top_topic
+    , pt1.top_topic_percent AS cluster_top_topic_percent
+    , pt1.cluster_primary_topics AS cluster_primary_topics_and_pct
 
-    , pt1.top_topic AS top_topic_overall
-    , pt1.top_topic_percent AS top_topic_percent_overall
-    , pr1.top_rating AS top_rating_overall
-    , top_rating_percent AS top_rating_percent_overall
-
-    , pt1.subreddits_in_cluster_count AS subreddits_in_cluster_count_overall
-    , tc.subreddits_in_cluster_germany
-
-    , tc.users_l7_cluster_total_germany
-    , ts.top_subreddits AS top_subreddits_overall
-    , tc.top_subreddits_germany
-
-    , top_keywords AS top_keywords_overall
-    , pt1.cluster_primary_topics AS cluster_primary_topics_overall
-    -- , pr1.* EXCEPT(k_0100_label, top_rating, subreddits_in_cluster_count, top_rating_percent)
 FROM tf_idf_single_row_per_cluster tf
-    LEFT JOIN top_subreddits_per_cluster AS ts
-        ON tf.cluster_id = ts.k_0100_label
-    LEFT JOIN top_subs_and_agg_germany AS tc
-        ON tf.cluster_id = tc.k_0100_label
+    LEFT JOIN cluster_compare_us_v_geo AS cc
+        ON tf.k_0100_label_name = cc.k_0100_label_name
 
-    -- topic & ratings from parent cluster ID
-    LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_top_topics` AS pt1
-        ON tf.cluster_id = pt1.k_0100_label
-    LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_top_ratings` AS pr1
-        ON tf.cluster_id = pr1.k_0100_label
+    -- Topic aggregates from parent cluster ID
+    LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_top_topics_manual` AS pt1
+        ON tf.k_0100_label_name = pt1.k_0100_label_name
 
-    -- Need this to join both the k=100 and k=400 labels
-    -- LEFT JOIN (
-    --     SELECT DISTINCT k_0100_label, k_0400_label
-    --     FROM `reddit-employee-datasets.david_bermejo.subclu_v0041_subreddit_clusters_c_a_full`
-    -- ) AS t
-    --     ON tf.cluster_id = t.k_0400_label
-        -- LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k400_top_topics` AS pt
-    --     ON tf.cluster_id = pt.k_0400_label
-    -- LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k400_top_ratings` AS pr
-    --     ON tf.cluster_id = pr.k_0400_label
+    -- Rating aggregates: don't use for now b/c they're not at the right grain
+    -- LEFT JOIN `reddit-employee-datasets.david_bermejo.subclu_v0041_cluster_k100_top_ratings` AS pr1
+    --     ON tf.cluster_id = pr1.k_0100_label
 
 ORDER BY 1
 ;
