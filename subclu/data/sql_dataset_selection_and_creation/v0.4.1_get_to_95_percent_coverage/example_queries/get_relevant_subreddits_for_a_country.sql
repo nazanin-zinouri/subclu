43ae3596839d2951ebed2b93b0a2b83cd3bc6186
David Bermejo <david.bermejo@reddit.com>
2022-05-26 15:59:51 -0500

    Add new query to find relevant countries for specific subreddits

--- subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/example_queries/get_relevant_subreddits_for_a_country.sql
+++ subclu/data/sql_dataset_selection_and_creation/v0.4.1_get_to_95_percent_coverage/example_queries/get_relevant_subreddits_for_a_country.sql
@@ -11,3 +11,22 @@ WHERE 1=1
 
 ORDER BY subreddit_rank_in_country ASC
 ;
+
+
+-- Find relevant countries for specific subreddits
+subclu_subreddit_relevance_beta_20220502
+SELECT *
+FROM `reddit-employee-datasets.david_bermejo.subclu_subreddit_relevance_beta_20220502`
+WHERE subreddit_name IN (
+    'soccer', 'cricket', 'fussball'
+)
+    AND (
+        geo_relevance_default = TRUE
+        OR relevance_percent_by_subreddit = TRUE
+        -- OR relevance_percent_by_country_standardized = TRUE
+        OR users_percent_by_subreddit_l28 >= 0.08
+        OR users_percent_by_country_standardized >= 3.0
+    )
+ORDER BY subreddit_name, users_percent_by_subreddit_l28 DESC
+LIMIT 1000
+;
