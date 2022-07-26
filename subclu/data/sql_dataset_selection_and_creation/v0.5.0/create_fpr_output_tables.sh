50f18f9eb0552f7a536ef973e28912c415b8ac6a
David Bermejo <david.bermejo@reddit.com>
2022-07-26 03:46:24 -0500

    Add bq commands to create FPR outputs in bigQuery tables

---
+++ subclu/data/sql_dataset_selection_and_creation/v0.5.0/create_fpr_output_tables.sh
@@ -0,0 +1,27 @@
+# Create tables with FPR outputs so that we can create a dashboard to
+#  review and share FPR outputs
+
+bq load \
+    --source_format=PARQUET \
+    --project_id=reddit-employee-datasets \
+    david_bermejo.subclu_v0050_fpr_summary \
+    "gs://i18n-subreddit-clustering/i18n_topic_model_batch/fpr/runs_test/2022-07-26_074854/df_fpr_qa_summary/*.parquet"
+
+
+bq load \
+    --source_format=PARQUET \
+    --project_id=reddit-employee-datasets \
+    david_bermejo.subclu_v0050_fpr_outputs \
+    "gs://i18n-subreddit-clustering/i18n_topic_model_batch/fpr/runs_test/2022-07-26_074854/df_fpr/*.parquet"
+
+bq load \
+    --source_format=PARQUET \
+    --project_id=reddit-employee-datasets \
+    david_bermejo.subclu_v0050_fpr_dynamic_clusters \
+    "gs://i18n-subreddit-clustering/i18n_topic_model_batch/fpr/runs_test/2022-07-26_074854/df_dynamic_clusters/*.parquet"
+
+bq load \
+    --source_format=PARQUET \
+    --project_id=reddit-employee-datasets \
+    david_bermejo.subclu_v0050_fpr_cluster_summary \
+    "gs://i18n-subreddit-clustering/i18n_topic_model_batch/fpr/runs_test/2022-07-26_074854/df_fpr_cluster_summary/*.parquet"
