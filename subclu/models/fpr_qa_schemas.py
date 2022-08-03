e76f7388ef5bf04e79e02577f705406d6f675558
David Bermejo <david.bermejo@reddit.com>
2022-08-03 10:16:24 -0500

    Fix: assign dtype by custom pa.dtypes

--- subclu/models/fpr_qa_schemas.py
+++ subclu/models/fpr_qa_schemas.py
@@ -166,67 +166,10 @@ def fpr_qa_schema(
             l_pa_schema.append(
                 pa.field(
                     sf_.name,
-                    d_pa_dtype_list_cols.get(sf_.field_type, bq_to_pa_dtype(sf_.field_type)),
+                    d_pa_dtype_list_cols.get(sf_.name, bq_to_pa_dtype(sf_.field_type)),
                     metadata={'description': sf_.description}
                 )
             )
-        fpr_qa_schema = pa.schema(
-            [
-                ('pt', pa.date32()),
-                ('geo_relevance_table', pa.string()),
-                ('qa_pt', pa.string()),
-                ('qa_table', pa.string()),
-                ('run_id', pa.string()),
-                ('geo_country_code', pa.string()),
-                ('country_name', pa.string()),
-                ('relevant_subreddit_id_count', pa.int64()),
-                ('bucket', pa.string()),
-                ('path_fpr_json', pa.string()),
-                ('path_df_fpr', pa.string()),
-                ('path_df_fpr_qa_summary', pa.string()),
-                ('path_df_fpr_cluster_summary', pa.string()),
-                ('seed_subreddit_ids', pa.list_(pa.string())),
-                ('seed_subreddit_ids_count', pa.int64()),
-                ('recommend_subreddit_ids', pa.list_(pa.string())),
-                ('recommend_subreddit_ids_count', pa.int64()),
-                ('orphan_or_exclude_seed_subreddit_ids_list', pa.list_(pa.string())),
-                ('orphan_or_exclude_seed_subreddit_ids_count', pa.int64()),
-                ('orphan_seed_subreddit_ids_list', pa.list_(pa.string())),
-                ('orphan_seed_subreddit_ids_count', pa.int64()),
-                ('orphan_recommend_subreddit_ids_list', pa.list_(pa.string())),
-                ('orphan_recommend_subreddit_ids_count', pa.int64()),
-                ('clusters_total', pa.int64()),
-                ('clusters_with_recommendations', pa.int64()),
-            ],
-            metadata={
-                'pt': 'Partition time for subreddit_lookup & crowdsourced topic & ratings',
-                'geo_relevance_table': 'Name of table used for geo-relevance scores',
-                'qa_pt': 'PT for QA decisions',
-                'qa_table': 'Table with QA logic',
-                'run_id': 'Run ID (timestamp) for this FPR',
-                'geo_country_code': 'Geo country code for relevance & FPR',
-                'country_name': 'Country name (based on geo_country_code)',
-                'relevant_subreddit_id_count': 'Num of subreddits relevant to country',
-                'bucket': 'Bucket with FPR output',
-                'path_fpr_json': 'Path for FPR JSON output',
-                'path_df_fpr': 'Path for dataframe with FPR output',
-                'path_df_fpr_qa_summary': 'Path for dataframe with FPR summary (this table!)',
-                'path_df_fpr_cluster_summary': 'Path for df with cluster-level summary',
-                'seed_subreddit_ids':
-                    "List of seed IDs. If user subscribes to a SEED we will rec posts from 'recommend list'",
-                'seed_subreddit_ids_count': 'Count of seed subreddit IDs. Note: EXCLUDES orphans.',
-                'recommend_subreddit_ids': 'List of subreddit IDs to recommend in country FPR',
-                'recommend_subreddit_ids_count': 'Count of subreddits to recommend in country FPR. NOTE: excludes orphans & do not recommend',
-                'orphan_or_exclude_seed_subreddit_ids_list': 'List of orphan or exclude SEED subreddit IDs',
-                'orphan_or_exclude_seed_subreddit_ids_count': 'Count of oprhan or exclude SEED subredit IDs',
-                'orphan_seed_subreddit_ids_list': "List of subreddits that are by themselves or can't be recommended to something else",
-                'orphan_seed_subreddit_ids_count': "Count of subreddits that can't be recommended",
-                'orphan_recommend_subreddit_ids_list': "List of subreddits that can be recommended, but are orphaned",
-                'orphan_recommend_subreddit_ids_count': "Count of subreddits that can be recommended but are by themselves",
-                'clusters_total': "Total num of clusters in country",
-                'clusters_with_recommendations': "Num of clusters that have 1+ subreddits to recommend",
-            },
-        )
         pa_schema = pa.schema(l_pa_schema)
         return pa_schema
 
