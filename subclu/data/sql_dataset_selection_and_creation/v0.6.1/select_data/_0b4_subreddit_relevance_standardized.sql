5e5ccfb40ee983096f63fad4ed55167c67f4dce2
David Bermejo <david.bermejo@reddit.com>
2022-11-04 19:35:40 -0500

    Select from local_score ETL instead of calculating geo-relevance score at run-time

--- subclu/data/sql_dataset_selection_and_creation/v0.6.1/select_data/_0b4_subreddit_relevance_standardized.sql
+++ subclu/data/sql_dataset_selection_and_creation/v0.6.1/select_data/_0b4_subreddit_relevance_standardized.sql
@@ -8,19 +8,14 @@
 --  >= 4 posts in L28 days
 --  >= 45 users (unique user screenviews) in L28 days
 
+DECLARE PARTITION_DATE DATE DEFAULT ${end_date};
+
 -- Lower these thresholds to get scores for low-activity subs
 DECLARE MIN_POSTS_L28_NOT_REMOVED NUMERIC DEFAULT 2;
 DECLARE MIN_USERS_L7 NUMERIC DEFAULT 10;
 
--- Set minimum thresholds for calculating standardized score
+-- Minimum thresholds for calculating standardized score are now in i18n ETL
 --  because some small islands screw up standard-dev scores
--- Rough percentiles:
---  p25:        10k
---  p50:        67k
---  p75:       610k
---  AVG:     2,700k (2.7 million)
-DECLARE MIN_USERS_IN_COUNTRY_FOR_STDEV DEFAULT 11000;  -- Min coutry size to calculate stdev
-DECLARE MIN_USERS_IN_COUNTRY DEFAULT 9000;  -- Min country size to SAVE to table. Monaco ~10k
 
 -- Set weights for combined score
 DECLARE STANDARD_VALUE_WHEN_STDEV_ZERO DEFAULT 9.0;
@@ -39,37 +34,12 @@ DECLARE E_MIN_USERS_PCT_BY_COUNTRY_STANDARDIZED DEFAULT 2.5;
 
 -- For final table output, exclude subs that have too few users from a country,
 --  otherwise each country could have 110k+ rows
-DECLARE MIN_USERS_IN_SUBREDDIT_BY_COUNTRY DEFAULT 3;
+DECLARE MIN_USERS_IN_SUBREDDIT_BY_COUNTRY DEFAULT 2;
 
 
 CREATE OR REPLACE TABLE `reddit-relevance.${dataset}.subclu_subreddit_geo_relevance_standardized_${run_id}`
 AS (
-    WITH users_mean_and_stdev AS (
-        -- get the mean, std, & country count for each subreddit
-        SELECT
-            subreddit_name
-            , subreddit_id
-
-            , COUNT(DISTINCT geo_country_code) as num_of_countries_with_visits_for_stdev_l28
-            , AVG(users_percent_by_country_l28) as users_percent_by_country_avg
-            , COALESCE(STDDEV(users_percent_by_country_l28), 0) as users_percent_by_country_stdev
-
-            -- , AVG(users_percent_by_subreddit) as users_percent_by_subreddit_avg
-            -- , STDDEV(users_percent_by_subreddit) as users_percent_by_subreddit_stdev
-        FROM `reddit-relevance.${dataset}.subclu_subreddit_geo_relevance_raw_${run_id}` as geo
-        WHERE
-            -- subreddit filters
-            posts_not_removed_l28 >= MIN_POSTS_L28_NOT_REMOVED
-            AND users_l7 >= MIN_USERS_L7
-            AND subreddit_name != 'profile'
-
-            -- country size filters
-            AND users_in_subreddit_from_country_l28 >= 1
-            AND total_users_in_country_l28 >= MIN_USERS_IN_COUNTRY_FOR_STDEV
-            -- AND geo_country_code IS NOT NULL
-        GROUP BY 1, 2
-    ),
-
+WITH
     standard_score_and_rank_per_country AS (
         SELECT
             geo.subreddit_id
@@ -77,41 +47,30 @@ AS (
             , geo.geo_country_code
 
             -- B-metric = users_percent_by_subreddit
-            , geo.users_percent_by_subreddit_l28
+            , geo.sub_dau_perc_l28 AS users_percent_by_subreddit_l28
             -- E-metric = users_percent_by_country_standardized
-            , CASE
-                WHEN (m.users_percent_by_country_stdev = 0) THEN STANDARD_VALUE_WHEN_STDEV_ZERO
-                ELSE (geo.users_percent_by_country_l28 - m.users_percent_by_country_avg) / m.users_percent_by_country_stdev
-            END AS users_percent_by_country_standardized
-            , geo.users_percent_by_country_l28
-            , ROW_NUMBER() OVER (PARTITION BY geo_country_code ORDER BY users_percent_by_country_l28 DESC) AS users_percent_by_country_rank
+            , geo.perc_by_country_sd AS users_percent_by_country_standardized
+            , geo.perc_by_country AS users_percent_by_country_l28
+            , ROW_NUMBER() OVER (PARTITION BY geo_country_code ORDER BY perc_by_country DESC) AS users_percent_by_country_rank
 
-            , geo.users_in_subreddit_from_country_l28
-            , geo.total_users_in_country_l28
-            , geo.total_users_in_subreddit_l28
+            , geo.sub_dau_l28 AS users_in_subreddit_from_country_l28
 
-            , m.* EXCEPT(subreddit_name, subreddit_id)
-            , geo.pt
-            , geo.users_l7
-            , geo.posts_not_removed_l28
+            , DATE(geo.pt) AS pt
+            , ca.users_l7
+            , ca.posts_not_removed_l28
 
-        FROM `reddit-relevance.${dataset}.subclu_subreddit_geo_relevance_raw_${run_id}` as geo
-            LEFT JOIN users_mean_and_stdev AS m
-                ON geo.subreddit_id = m.subreddit_id
+        FROM `data-prod-165221.i18n.community_local_scores` as geo
+            LEFT JOIN `reddit-relevance.${dataset}.subclu_subreddit_candidates_${run_id}` AS ca
+              ON geo.subreddit_id = ca.subreddit_id
 
         WHERE
+            DATE(geo.pt) = PARTITION_DATE
+
             -- subreddit filters
-            posts_not_removed_l28 >= MIN_POSTS_L28_NOT_REMOVED
+            AND posts_not_removed_l28 >= MIN_POSTS_L28_NOT_REMOVED
             AND users_l7 >= MIN_USERS_L7
             AND geo.subreddit_name != 'profile'
 
-            -- country filters
-            AND users_in_subreddit_from_country_l28 >= 1
-            AND total_users_in_country_l28 >= MIN_USERS_IN_COUNTRY
-
-            -- get % for visits from a null country b/c it'd be good to know
-            --  if there are subs that have a high # of visits w/o IP addresses
-            -- AND geo_country_code IS NOT NULL
     ),
     merged_default_and_standardized_scores AS (
         SELECT
@@ -142,10 +101,7 @@ AS (
             , s.users_percent_by_country_standardized
             , s.users_in_subreddit_from_country_l28
 
-            , s.num_of_countries_with_visits_for_stdev_l28
             , s.users_percent_by_country_l28
-            , users_percent_by_country_avg
-            , users_percent_by_country_stdev
             , COALESCE(s.pt, base.pt) AS pt
 
         FROM standard_score_and_rank_per_country AS s
