0cc21eec167bec49b46a8246674178360304b013
David Bermejo <david.bermejo@reddit.com>
2022-11-11 13:01:21 -0600

    Update logic with new ratings & topic_v2

--- subclu/data/sql_dataset_selection_and_creation/taxonomy_labels/labels_02.2-2022-11-10-combine_taxonomy_and_reddit_vault_labels_NEW_cau_preds_vw.sql
+++ subclu/data/sql_dataset_selection_and_creation/taxonomy_labels/labels_02.2-2022-11-10-combine_taxonomy_and_reddit_vault_labels_NEW_cau_preds_vw.sql
@@ -8,6 +8,7 @@
 -- OPTIONS(
 --     description="View that combines all sources for topic & rating labels: curator, crowd, & CA model. Wiki: https://reddit.atlassian.net/wiki/spaces/DataScience/pages/2389278980/Content+Analytics+Key+Tables"
 -- )
+-- AS (
 
 WITH all_subreddit_labels AS (
     SELECT
@@ -20,44 +21,59 @@ WITH all_subreddit_labels AS (
         -- Booleans to indicate source for RATING
         , IF(c.curator_rating IS NULL, 0, 1) AS curator_rating_tag
         , IF(t.rating_short IS NULL, 0, 1) AS crowd_rating_tag
-        , IF(p.predicted_rating IS NULL, 0, 1) AS model_rating_tag
+        , IF(pr.rating_1 IS NULL, 0, 1) AS model_rating_tag
         -- When deciding the subreddit rating we take the first available rating in this order:
         --  curator > crowd > predicted
-         , CASE
+        , CASE
             WHEN c.curator_rating_short IS NOT NULL THEN c.curator_rating_short
             WHEN t.rating_short IS NOT NULL THEN t.rating_short
-            ELSE p.predicted_rating
+            ELSE pr.rating_1
         END AS subreddit_rating
+        -- Taxonomy rating -> curated or crowd. Use it as labels for modeling
+        , CASE
+            WHEN c.curator_rating_short IS NOT NULL THEN c.curator_rating_short
+            WHEN t.rating_short IS NOT NULL THEN t.rating_short
+            ELSE NULL
+        END AS taxonomy_rating
+        , c.curator_rating_name
         , c.curator_rating_short AS curator_rating
         , t.rating_short AS crowd_rating
-        , p.predicted_rating
-        , rating_prediction_score
+        , pr.rating_1 AS predicted_rating
+        , pr.rating_score_1 AS rating_prediction_score
         -- We assign the rating score as 1 (the maximum) if the curator or crowd label is available
         , CASE
             WHEN (c.curator_rating IS NOT NULL) OR (t.rating_short IS NOT NULL) THEN 1.0
-            ELSE rating_prediction_score
+            ELSE pr.rating_score_1
         END AS rating_score
 
         -- Booleans to indicate source for TOPIC
         , IF(c.curator_topic IS NULL, 0, 1) AS curator_topic_tag
+        , IF(c.curator_topic_v2 IS NULL, 0, 1) AS curator_topic_v2_tag
         , IF(t.primary_topic IS NULL, 0, 1) AS crowd_topic_tag
-        , IF(p.predicted_topic IS NULL, 0, 1) AS model_topic_tag
+        , IF(pt.topic_1 IS NULL, 0, 1) AS model_topic_tag
         -- This is the order for deciding the "true" topic:
         --   curator > crowd > predicted
         -- ("true" because topics can be subjective AND not mutually exclusive)
         , CASE
             WHEN c.curator_topic IS NOT NULL THEN c.curator_topic
             WHEN t.primary_topic IS NOT NULL THEN t.primary_topic
-            ELSE p.predicted_topic
+            ELSE pt.topic_1
         END AS subreddit_topic
+        -- Taxonomy rating -> curated or crowd. Use it as labels for modeling
+        , CASE
+            WHEN c.curator_topic IS NOT NULL THEN c.curator_topic
+            WHEN t.primary_topic IS NOT NULL THEN t.primary_topic
+            ELSE NULL
+        END AS taxonomy_topic
         , c.curator_topic AS curator_topic
+        , c.curator_topic_v2
         , t.primary_topic AS crowd_topic
-        , predicted_topic
-        , topic_prediction_score
+        , pt.topic_1 AS predicted_topic
+        , pt.topic_score_1 AS topic_prediction_score
         -- We assign the topic score as 1 (the maximum) if the curator or crowd label is available
         , CASE
             WHEN (c.curator_topic IS NOT NULL) OR (t.primary_topic IS NOT NULL) THEN 1.0
-            ELSE topic_prediction_score
+            ELSE pt.topic_score_1
         END AS topic_score
 
         , slo.subscribers
@@ -71,15 +87,36 @@ WITH all_subreddit_labels AS (
         WHERE dt = (CURRENT_DATE() - 2)
             AND NOT REGEXP_CONTAINS(LOWER(name), r'^u_.*')
     ) AS slo
-        LEFT JOIN `data-prod-165221.cnc.subreddit_metadata_lookup` AS t
+        LEFT JOIN (
+            SELECT DISTINCT
+                m1.subreddit_id,
+                subreddit_name,
+                survey_version,
+                primary_topic,
+                rating_short,
+                verification_timestamp
+            FROM
+                `data-prod-165221.cnc.subreddit_metadata_lookup` AS m1
+            INNER JOIN (
+                    SELECT
+                        subreddit_id,
+                        MAX(pt) AS ts
+                    FROM `data-prod-165221.cnc.subreddit_metadata_lookup`
+                    GROUP BY 1
+                    ) AS m2
+                ON m1.pt = m2.ts
+                AND m1.subreddit_id = m2.subreddit_id
+            WHERE survey_version = 'v5'
+        ) AS t
             ON slo.subreddit_id = t.subreddit_id
-        LEFT JOIN `reddit-employee-datasets.anna_scaramuzza.reddit_vault_predictions_20220411` AS p
-            ON slo.subreddit_id = p.subreddit_id
+        LEFT JOIN `reddit-employee-datasets.anna_scaramuzza.reddit_vault_all_topics_inference_20220929` AS pt
+            ON slo.subreddit_id = pt.subreddit_id
+        LEFT JOIN `reddit-employee-datasets.anna_scaramuzza.reddit_vault_all_ratings_inference_20220929` AS pr
+            ON slo.subreddit_id = pr.subreddit_id
         LEFT JOIN `reddit-employee-datasets.david_bermejo.taxonomy_curated_labels` AS c
             ON slo.subreddit_id = c.subreddit_id
 
     WHERE 1=1
-        AND t.pt = CURRENT_DATE() - 2
 )
 
 SELECT *
