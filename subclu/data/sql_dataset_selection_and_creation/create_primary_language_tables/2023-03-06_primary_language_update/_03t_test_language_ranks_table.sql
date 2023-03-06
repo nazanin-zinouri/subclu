e00d390095c6e885f6d3d19c31710e032ec7de9d
David Bermejo <david.bermejo@reddit.com>
2023-03-06 12:22:22 -0600

    Check top language in test subreddits

--- subclu/data/sql_dataset_selection_and_creation/create_primary_language_tables/2023-03-06_primary_language_update/_03t_test_language_ranks_table.sql
+++ subclu/data/sql_dataset_selection_and_creation/create_primary_language_tables/2023-03-06_primary_language_update/_03t_test_language_ranks_table.sql
@@ -11,3 +11,22 @@ FROM `reddit-employee-datasets.david_bermejo.subreddit_language_rank_20230306` A
 WHERE language_rank = 1
     AND lan.thing_type = 'posts_and_comments'
 ;
+
+
+
+-- Check top languages for test subreddits
+SELECT * EXCEPT(thing_type)
+FROM `reddit-employee-datasets.david_bermejo.subreddit_language_rank_20230305` AS lan
+
+WHERE language_rank <= 2
+    AND lan.thing_type = 'posts_and_comments'
+    AND subreddit_name IN (
+        'de', 'mexico', 'meirl', 'ich_iel'
+        , 'india'
+        , 'france', 'rance'
+        , 'czech', 'prague', 'sweden'
+        , 'japan', 'china_irl', 'newsokunomoral'
+        , 'ligamx', 'absoluteunits', 'aww'
+    )
+ORDER BY subreddit_name, language_rank
+;
