b941619b15fc7c518448f9f282c798d3fa236b84
David Bermejo <david.bermejo@reddit.com>
2023-04-17 13:26:19 -0500

    Fix: increase view window + add users included in actual send

--- subclu/pn_models/_01.1_select_users_test_de_campaign_only.sql
+++ subclu/pn_models/_01.1_select_users_test_de_campaign_only.sql
@@ -3,7 +3,8 @@
 --   - remove users who only viewed one subreddit
 --   - keep users with an iOS or Android event
 DECLARE PT_DT DATE DEFAULT "2022-12-01";
-DECLARE PT_WINDOW_START DATE DEFAULT PT_DT - 13;
+-- Expand to 30 days total to get at least 1 month's worth of data given that in the prev model 1 month was the minimum
+DECLARE PT_WINDOW_START DATE DEFAULT PT_DT - 29;
 
 DECLARE TARGET_COUNTRIES DEFAULT [
     "DE", "AT", "CH"
@@ -16,8 +17,14 @@ SELECT
     , PT_WINDOW_START AS pt_window_start
     , v.subreddit_id
     , v.subreddit_name
-    , v.user_id
+    , COALESCE(v.user_id, act.user_id) AS user_id
     , g.geo_country_code
+    -- Testing column to debug missing users
+    , IF(
+        (act.user_id IS NOT NULL) AND (v.user_id IS NULL) ,
+        1,
+        0
+    ) AS user_in_actual_but_missing_from_new
 
     , COUNT(DISTINCT post_id) AS view_and_consume_unique_count
     , COUNT(DISTINCT(IF(v.action='consume', post_id, NULL))) AS consume_unique_count
@@ -35,18 +42,38 @@ FROM (
         DATE(pt) = PT_DT
         AND geo_country_code IN UNNEST(TARGET_COUNTRIES)
 ) AS g
-    INNER JOIN `data-prod-165221.fact_tables.post_consume_post_detail_view_events` AS v
+    INNER JOIN (
+        SELECT
+          subreddit_id
+          , subreddit_name
+          , user_id
+          , post_id
+          , app_name
+          , action
+        FROM `data-prod-165221.fact_tables.post_consume_post_detail_view_events`
+        WHERE DATE(pt) BETWEEN PT_WINDOW_START AND PT_DT
+            AND user_id IS NOT NULL
+            AND subreddit_name IN (
+                -- 1st sub is a target from a campaign & following ones are most similar based on ft2 embeddings:
+                'de', 'fragreddit', 'ich_iel'
+            )
+            AND action IN ('consume', 'view')
+    ) AS v
         on g.user_id = v.user_id
 
+    FULL OUTER JOIN (
+        -- Add Actual SENT users in case they don't match my selecting criteria
+        SELECT user_id
+        FROM `reddit-growth-prod.generated_one_offs.20221201191705_elizabethpollard_de_de_der_topbeitrag_diese_woche_62384`
+        UNION ALL
+        SELECT user_id
+        FROM `reddit-growth-prod.generated_one_offs.20221201193812_elizabethpollard_de_de_der_topbeitrag_diese_woche_32358`
+    ) act
+        ON g.user_id = act.user_id
+
     -- TODO(djb): join to get only users who have at least 1 ios or android event in ANY sub
-WHERE DATE(v.pt) BETWEEN PT_WINDOW_START AND PT_DT
-    AND v.user_id IS NOT NULL
-    AND v.subreddit_name IN (
-        -- 1st sub is a target from a campaign & following ones are most similar based on ft2 embeddings:
-        , 'de', 'fragreddit', 'ich_iel'
-    )
-    AND action IN ('consume', 'view')
-GROUP BY 1,2,3,4,5,6
+WHERE 1=1
+GROUP BY 1,2,3,4,5,6,7
 );
 
 
