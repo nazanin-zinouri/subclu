d6d3ac8dea754b8f2aba9facc5ac478b730daff8
David Bermejo <david.bermejo@reddit.com>
2023-05-09 18:34:25 -0500

    Fix duplicates in user-features table

--- subclu/pn_models/_b.01.00-create_table_with_user_level_features.sql
+++ subclu/pn_models/_b.01.00-create_table_with_user_level_features.sql
@@ -1,13 +1,13 @@
 -- B. Create table with Consumes in L30 + PN activity in L7 days
+--   ETA: 2-4 minutes (with geo)
 DECLARE PT_FEATURES DATE DEFAULT CURRENT_DATE() - 2;
 DECLARE PT_PN_WINDOW_START DATE DEFAULT PT_FEATURES - 7;
--- DECLARE PT_CONSUMES_START DATE DEFAULT PT_FEATURES - 2;  -- 29
 
 
 -- ==================
 -- Only need to create the first time we run it
 -- === OR REPLACE
--- CREATE OR REPLACE TABLE `reddit-employee-datasets.david_bermejo.pn_ft_user_20230502`
+-- CREATE TABLE `reddit-employee-datasets.david_bermejo.pn_ft_user_20230509`
 -- PARTITION BY pt
 -- AS (
 
@@ -15,19 +15,20 @@ DECLARE PT_PN_WINDOW_START DATE DEFAULT PT_FEATURES - 7;
 -- After table is created, we can delete a partition & update it
 -- ===
 DELETE
-    `reddit-employee-datasets.david_bermejo.pn_ft_user_20230502`
+    `reddit-employee-datasets.david_bermejo.pn_ft_user_20230509`
 WHERE
     pt = PT_FEATURES
 ;
 
 -- Insert latest data
-INSERT INTO `reddit-employee-datasets.david_bermejo.pn_ft_user_20230502`
+INSERT INTO `reddit-employee-datasets.david_bermejo.pn_ft_user_20230509`
 (
 
 WITH post_consumes_agg AS (
     SELECT
         COALESCE(sv.entity_id, pc.user_id) AS user_id
         , COALESCE(sv.feature_value, 0) AS screen_view_count_14d
+
         , SUM(num_post_consumes) AS num_post_consumes
         , SUM(num_post_consumes_home) AS num_post_consumes_home
         , SUM(num_post_consumes_community) AS num_post_consumes_community
@@ -41,10 +42,15 @@ WITH post_consumes_agg AS (
         SELECT entity_id, feature_value
         FROM `data-prod-165221.user_feature_platform.screen_views_count_over_14_days_v1`
         WHERE DATE(pt) = PT_FEATURES
+            AND entity_id IS NOT NULL
     ) AS sv
-        FULL OUTER JOIN `data-prod-165221.video.post_consumes_30d_agg` AS pc
+        FULL OUTER JOIN (
+            SELECT *
+            FROM `data-prod-165221.video.post_consumes_30d_agg`
+            WHERE DATE(pt) = PT_FEATURES
+                AND user_id IS NOT NULL
+        ) AS pc
             ON pc.user_id = sv.entity_id
-    WHERE DATE(pc.pt) = PT_FEATURES
     GROUP BY 1,2
 )
 , user_actions_t7 AS (
@@ -63,16 +69,23 @@ WITH post_consumes_agg AS (
           ON pne.user_id = c.user_id
     WHERE
         DATE(pne.pt) BETWEEN PT_PN_WINDOW_START AND PT_FEATURES
-        AND NOT REGEXP_CONTAINS(notification_type, "email")
-        AND receive_endpoint_timestamp IS NOT NULL
+        AND NOT REGEXP_CONTAINS(pne.notification_type, "email")
+        AND pne.receive_endpoint_timestamp IS NOT NULL
+        AND pne.user_id IS NOT NULL
   GROUP BY user_id
 )
 
+-- Final select to create/insert
 SELECT
     PT_FEATURES AS pt
     , pc.user_id
+    , g.geo_country_code AS user_geo_country_code
+    , LN(1 + COALESCE(pc.screen_view_count_14d, 0)) AS screen_view_count_14d_log
     , ua.* EXCEPT(user_id)
     , pc.num_post_consumes
+    , LN(1 + COALESCE(pc.num_post_consumes, 0)) AS log_post_consumes
+    , LN(1 + COALESCE(num_post_consumes_home, 0)) AS log_num_post_consumes_home
+
     , SAFE_DIVIDE(num_post_consumes_home, num_post_consumes) AS pct_post_consumes_home
     , SAFE_DIVIDE(num_post_consumes_community, num_post_consumes) AS pct_post_consumes_community
     , SAFE_DIVIDE(num_post_consumes_post_detail, num_post_consumes) AS pct_post_consumes_post_detail
@@ -83,4 +96,14 @@ SELECT
 FROM post_consumes_agg AS pc
     LEFT JOIN user_actions_t7 AS ua
         ON pc.user_id = ua.user_id
-); -- Close CREATE parens
+    LEFT JOIN (
+            SELECT
+                user_id
+                , geo_country_code
+            FROM `data-prod-165221.channels.user_geo_6mo_lookback`
+            WHERE
+                DATE(pt) = PT_FEATURES
+                AND user_id IS NOT NULL
+        ) AS g
+            ON pc.user_id = g.user_id
+); -- Close CREATE/INSERT parens
