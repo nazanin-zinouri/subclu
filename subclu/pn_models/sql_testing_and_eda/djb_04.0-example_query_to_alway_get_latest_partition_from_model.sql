99ae4c323a8b51649bfad3a9bbea0f9821fe2992
David Bermejo <david.bermejo@reddit.com>
2023-05-24 17:45:12 -0500

    Add sample query to check potential target count

--- subclu/pn_models/sql_testing_and_eda/djb_04.0-example_query_to_alway_get_latest_partition_from_model.sql
+++ subclu/pn_models/sql_testing_and_eda/djb_04.0-example_query_to_alway_get_latest_partition_from_model.sql
@@ -9,11 +9,8 @@ SELECT
 FROM `reddit-growth-prod.pn_targeting.pn_model_subreddit_user_click_v1`
 WHERE
     pt = (
-        SELECT
-            DATE(PARSE_TIMESTAMP("%Y%m%d", MAX(partition_id)))
-        FROM
-          `reddit-growth-prod.pn_targeting`.INFORMATION_SCHEMA.PARTITIONS
-        WHERE
-          table_name = "pn_model_subreddit_user_click_v1"
+        SELECT DATE(PARSE_TIMESTAMP("%Y%m%d", MAX(partition_id)))
+        FROM `reddit-growth-prod.pn_targeting`.INFORMATION_SCHEMA.PARTITIONS
+        WHERE table_name = "pn_model_subreddit_user_click_v1"
     )
 ;
