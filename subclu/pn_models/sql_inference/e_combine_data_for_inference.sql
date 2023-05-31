0e73e02db5ccc370dab6b8b470c7ac12ea316426
David Bermejo <david.bermejo@reddit.com>
2023-05-31 11:56:15 -0500

    Update E (combine features) for inference to match prod

--- subclu/pn_models/sql_inference/e_combine_data_for_inference.sql
+++ subclu/pn_models/sql_inference/e_combine_data_for_inference.sql
@@ -1,56 +1,51 @@
--- This query was run on a notebook for training
--- %%time
--- %%bigquery df_train_raw --project data-prod-165221
+-- E. Data for model TRAINING (or inference)
+--   ETA: 2 mins. For top 1k subreddits
+--   ETA: 4 mins. For top 26k subreddits. Slot time: 3 days
+-- Combine data into flat format so it's easy to replicate & to export to GCS
 
--- E. Select lables and input data to train models
---   v1 Start with training labels to skip inference table
+-- For inference: select a SINGLE date
+DECLARE PT_FEATURES DATE DEFAULT '2023-05-29';
 
-DECLARE PT_FEATURES DEFAULT [
-    DATE('2022-12-01')
-    , DATE('2023-02-19')
-    , DATE('2023-02-28')
-    , DATE('2023-04-17')
-    , DATE('2023-04-24')
-    , DATE('2023-05-04')
-    , DATE('2023-05-07')
-    , DATE('2023-05-08')
-    , DATE('2023-05-09')
-    , DATE('2023-05-11')
-];
--- Need explicit max & min for legacy user cohorts
-DECLARE PT_FEATURES_MAX DEFAULT TIMESTAMP((SELECT MAX(dt) FROM UNNEST(PT_FEATURES) AS dt));
-DECLARE PT_FEATURES_MIN DEFAULT TIMESTAMP((SELECT MIN(dt) FROM UNNEST(PT_FEATURES) AS dt));
+-- For training: select an ARRAY of DATES
+-- DECLARE PT_FEATURES DEFAULT [
+--     DATE('2023-05-29')
+-- ];
+-- -- Need explicit max & min for legacy user cohorts
+-- DECLARE PT_FEATURES_MAX DEFAULT TIMESTAMP((SELECT MAX(dt) FROM UNNEST(PT_FEATURES) AS dt));
+-- DECLARE PT_FEATURES_MIN DEFAULT TIMESTAMP((SELECT MIN(dt) FROM UNNEST(PT_FEATURES) AS dt));
+
+
+-- ==================
+-- Only need to create the first time we run it
+-- === OR REPLACE
+-- CREATE TABLE `reddit-employee-datasets.david_bermejo.pn_ft_all_20230530`
+-- CLUSTER BY pt, target_subreddit_id
+-- AS (
+
+-- ==================
+-- After table is created, we can delete a partition & update it
+-- ===
+DELETE
+    `reddit-employee-datasets.david_bermejo.pn_ft_all_20230530`
+WHERE
+    pt = PT_FEATURES
+;
+-- Insert latest data
+INSERT INTO `reddit-employee-datasets.david_bermejo.pn_ft_all_20230530`
+(
+
 
 WITH
-train_labels AS (
-    SELECT
-        pt_send
-        , (pt_send - 1) AS pt
-        , user_id
-        , target_subreddit
-        , t.send
-        , t.receive
-        , t.click
-        , t.pn_id
-        , t.correlation_id
-    FROM `reddit-employee-datasets.david_bermejo.pn_training_data_20230515` AS t
-    WHERE (COALESCE(t.receive, 0) + COALESCE(t.click, 0)) >= 1
-        AND (pt_send - 1) IN UNNEST(PT_FEATURES)
-
-        -- TODO(djb): go back and fix dupes later: EXCLUDE campaigns that have duplicate data??
-        -- AND pt_send NOT IN ('2023-03-01', '2023-05-10')
-)
-
-, subreddit_ft AS (
+subreddit_ft AS (
     -- Query A
     SELECT
         s.* EXCEPT(
-            relevant_geo_country_codes, relevant_geo_country_code_count
+            subreddit_name, relevant_geo_country_codes, relevant_geo_country_code_count
             -- TODO(djb): try encoding rating & topics later or let model ecode them
             , over_18, curator_rating, curator_topic_v2
         )
     FROM `reddit-employee-datasets.david_bermejo.pn_ft_subreddits_20230525` AS s
-    WHERE s.pt IN UNNEST(PT_FEATURES)
+    WHERE s.pt = PT_FEATURES  -- IN UNNEST(PT_FEATURES)
 
     -- For testing, we can limit to only the top subreddits
     -- ORDER BY users_l7 DESC
@@ -60,138 +55,97 @@ train_labels AS (
     -- Query C
     SELECT
         us.* EXCEPT(pt_window_start)
-        -- For inference, keep raw country
 
     FROM `reddit-employee-datasets.david_bermejo.pn_ft_user_subreddit_20230529` AS us
 
-    WHERE us.pt IN UNNEST(PT_FEATURES)
-        -- TODO(djb): for Inference Only keep users from target geos
+    WHERE us.pt = PT_FEATURES  -- IN UNNEST(PT_FEATURES)
+        -- For Inference Only keep users from target geos. Assume that geo has been filtered upstream
         -- AND COALESCE(us.user_geo_country_code, "") IN UNNEST(TARGET_COUNTRY_CODES)
 )
 , user_ft AS (
     -- Query B
-    -- TODO(djb): append cohorts to user-level table from the start!
     SELECT
-        CASE
-            WHEN cl.legacy_user_cohort = 'new' THEN 1
-            WHEN cl.legacy_user_cohort = 'resurrected' THEN 2
-            WHEN cl.legacy_user_cohort = 'casual' THEN 3
-            WHEN cl.legacy_user_cohort IS NULL THEN 4  -- '_missing_' or 'dead'
-            WHEN cl.legacy_user_cohort = 'core' THEN 5
-            ELSE 0
-        END AS legacy_user_cohort_ord
-        , u.*
+        u.*
     FROM (
-            SELECT *
-            FROM `reddit-employee-datasets.david_bermejo.pn_ft_user_20230529`
-            WHERE pt IN UNNEST(PT_FEATURES)
-        ) AS u
-        LEFT JOIN (
-            -- get USER cohort, Legacy
-            SELECT user_id, legacy_user_cohort, pt
-            FROM `data-prod-165221.attributes_platform.user_rolling_legacy_user_cohorts`
-            WHERE pt BETWEEN PT_FEATURES_MIN AND PT_FEATURES_MIN
-        ) AS cl
-            ON u.user_id = cl.user_id AND cl.pt = TIMESTAMP(u.pt)
+        SELECT *
+        FROM `reddit-employee-datasets.david_bermejo.pn_ft_user_20230529`
+        WHERE pt = PT_FEATURES  -- IN UNNEST(PT_FEATURES)
+    ) AS u
 )
-# , final_table AS (
-#     SELECT
-#         -- These index columns are needed for the final outputs (not inference)
-#         COALESCE(u.pt, us.pt) AS pt
-#         , us.target_subreddit_id
-#         , us.target_subreddit
-#         , COALESCE(u.user_id, us.user_id) AS user_id
-#         , COALESCE(u.user_geo_country_code, us.user_geo_country_code) AS user_geo_country_code
-
-#         -- The rest of the columns should be used for modeling inference
-#         , u.* EXCEPT(pt, user_id, user_geo_country_code)
-#         , us.* EXCEPT(pt, user_id, target_subreddit, target_subreddit_id, user_geo_country_code)
-
-#         , s.* EXCEPT(subreddit_id, subreddit_name, pt)
-
-#     -- TODO(djb): training: Start with User-level features, b/c we might need to fill nulls for MISSING target_subreddits
-#     --   For inference, we might want to start with user<>subreddit features and only do inner join with subreddit features
-#     FROM user_ft AS u
-#         LEFT JOIN user_subreddit_ft AS us
-#             ON us.user_id = u.user_id  AND us.pt = u.pt
-#         -- Subreddit features. limit subreddits to score by using inner join
-#         LEFT JOIN subreddit_ft AS s
-#             ON us.target_subreddit_id = s.subreddit_id AND us.pt = s.pt
-# )
-, final_table_with_labels AS (
+, final_table AS (
     SELECT
-        -- Training labels
-        t.pt_send
-        , t.send
-        , t.receive
-        , t.click
-        , t.pn_id
-        , t.correlation_id
-
         -- These index columns are needed for the final outputs (not inference)
-        , COALESCE(u.pt, us.pt) AS pt
+        us.pt
         , us.target_subreddit_id
-        , COALESCE(LOWER(t.target_subreddit), us.target_subreddit) AS target_subreddit
-        , COALESCE(t.user_id, u.user_id, us.user_id) AS user_id
+        , us.target_subreddit
+        , us.user_id
         , COALESCE(u.user_geo_country_code, us.user_geo_country_code) AS user_geo_country_code
 
         -- The rest of the columns should be used for modeling inference
         , u.* EXCEPT(pt, user_id, user_geo_country_code)
         , us.* EXCEPT(pt, user_id, target_subreddit, target_subreddit_id, user_geo_country_code)
 
-        -- Exclude subreddit features that are raw counts (log features should be better)
-        , s.users_log_l28
-        , s.seo_users_pct_l28
-        , s.loggedin_users_pct_l28
-        , s.ios_users_pct_l28
-        , s.android_users_pct_l28
-#         , s.* EXCEPT(
-#             subreddit_id, subreddit_name, pt
-#             , votes_l7, votes_l28
-#             , ios_users_l28, android_users_l28
-#             , loggedin_users_l28, seo_users_l28
-#             , comments_l7, comments_l28
-#             , users_l7, users_l14, users_l28
-#             , posts_l7, posts_l28
-#         )
-    FROM train_labels AS t
+        , s.* EXCEPT(subreddit_id, pt)
+
+    -- TODO(djb): training: Start with User-level features, b/c we might need to fill nulls for MISSING target_subreddits
+    --   For inference, we might want to start with user<>subreddit features and only do inner join with subreddit features
+    FROM user_subreddit_ft AS us
         LEFT JOIN user_ft AS u
-            ON t.pt = u.pt AND t.user_id = u.user_id
-        LEFT JOIN user_subreddit_ft AS us
-            ON t.user_id = us.user_id
-                AND t.pt = us.pt
-                AND LOWER(t.target_subreddit) = us.target_subreddit
-        -- Subreddit features. limit subreddits to score by using inner join
+            ON us.user_id = u.user_id  AND us.pt = u.pt
+        -- Subreddit features
         LEFT JOIN subreddit_ft AS s
-            ON LOWER(t.target_subreddit) = s.subreddit_name
-                AND t.pt = s.pt
+            ON us.target_subreddit_id = s.subreddit_id AND us.pt = s.pt
 )
 
+SELECT *
+FROM final_table
+);  -- Close CREATE/INSERT parens
 
-SELECT
-    COALESCE(click, 0) AS click
-    , COALESCE(subscribed, 0) AS subscribed
-    , * EXCEPT(
-        click, subscribed
-        -- Other columns that have log transform OR percent
-        , num_post_consumes_30, num_post_consumes_home_30
-        , num_post_consumes_community_30, num_post_consumes_post_detail_30
-        , num_post_consumes_nsfw_30
-        , user_clicks_pn_t7
-        , user_clicks_trnd_t7
-        , screen_view_count_14d
-        , us_trend_pn_receive_l14
-        , us_screenviews_l14
-        , us_post_screenviews_l14
-        , us_distinct_posts_viewed_l14
-    )
-FROM final_table_with_labels
-WHERE 1=1
-    -- Drop users without recent activity because there's no data for model to learn from
-    AND (
-        click = 1
-        AND user_receives_pn_t30 IS NOT NULL
-        OR log_post_consumes_30 IS NOT NULL
-    )
-ORDER BY receive DESC, click DESC, num_post_consumes_30 DESC
-;
+
+-- ============
+-- Test CTEs
+-- ===
+-- SELECT
+--     (SELECT COUNT(*) FROM subreddit_ft) AS subreddit_ft_rows
+
+--     , (SELECT COUNT(*) FROM user_subreddit_ft) AS user_subreddit_ft_rows
+--     , (SELECT COUNT(DISTINCT user_id) FROM user_subreddit_ft) AS user_subreddit_ft_user_count
+--     , (SELECT COUNT(DISTINCT target_subreddit_id) FROM user_subreddit_ft) AS user_subreddit_ft_subreddit_count
+
+--     , (SELECT COUNT(*) FROM user_ft) AS user_ft_rows
+--     , (SELECT COUNT(DISTINCT user_id) FROM user_ft) AS user_ft_user_count
+
+--     , (SELECT COUNT(*) FROM final_table) AS final_table_rows
+--     , (SELECT COUNT(DISTINCT user_id) FROM final_table) AS final_table_user_count
+-- ;
+
+
+-- SELECT *
+-- FROM subreddit_per_user_count;
+
+-- SELECT *
+-- FROM user_subreddit_ft;
+
+
+
+-- ============
+-- Test clicks & receives on full table
+-- ===
+-- With this sample, we see that ROW is about average for clicks, but null/`MISSING` is the worst performing geo group
+--   For now, don't run inference on ROW or NULL because we won't send PNS to these users
+-- SELECT
+--     user_geo_country_code_top
+--     -- , subscribed
+
+--     , APPROX_QUANTILES(user_receives_pn_t7, 100)[OFFSET(50)] AS user_receives_pn_t7_median
+--     , APPROX_QUANTILES(user_clicks_pn_t7, 100)[OFFSET(80)] AS user_clicks_pn_t7_p80
+--     , APPROX_QUANTILES(user_clicks_pn_t7, 100)[OFFSET(90)] AS user_clicks_pn_t7_p90
+--     , APPROX_QUANTILES(user_clicks_pn_t7, 100)[OFFSET(95)] AS user_clicks_pn_t7_p95
+--     -- , AVG(user_receives_pn_t7) AS user_receives_pn_t7_avg
+--     , COUNT(DISTINCT user_id) AS user_count
+--     , COUNT(*) AS row_count
+-- FROM `reddit-employee-datasets.david_bermejo.pn_ft_all_20230509`
+-- WHERE pt = "2023-05-07"
+-- GROUP BY 1 -- , 2
+-- ORDER BY user_clicks_pn_t7_p90 DESC, user_count DESC
+-- ;
